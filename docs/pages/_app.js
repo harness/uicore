950bff216ef8b18c9d99b792acb3cff41f985db7
Tan Nhu <tannhu@gmail.com>
2019-09-10 01:12:26 -0700

    Add more icons

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -5,55 +5,30 @@ import React from 'react'
 import App from 'next/app'
 import Head from 'next/head'
 import Nav from '../shared/Nav'
+import Layout from '../shared/Layout'
 
-class DocsApp extends App {
+export default class DocsApp extends App {
   render() {
-    const { Component, pageProps } = this.props
+    const {
+      Component,
+      pageProps,
+      router: { route }
+    } = this.props
+    const title = route.replace(/\W/g, '')
 
     return (
-      <div>
+      <>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
+          <title>UI Platform - {title.charAt(0).toUpperCase() + title.slice(1)}</title>
           <link rel="stylesheet" href="/static/index.css" />
-          <link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.1.2/dist/base.min.css" />
-          <link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.1.2/dist/components.min.css" />
-          <link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.1.2/dist/utilities.min.css" />
-          <title>Some title</title>
         </Head>
 
-        <Nav />
-
-        <main>
+        <Layout>
           <Component {...pageProps} />
-        </main>
-
-        <style jsx>{`
-          div {
-            background-color: #f5f8fa;
-            display: flex;
-            flex-direction: row;
-            margin: auto;
-            max-width: 1100px;
-            min-height: 100vh;
-          }
-
-          div > * {
-            flex-grow: 0;
-            flex-shrink: 0;
-          }
-
-          main {
-            flex-basis: 830px;
-            align-items: flex-start;
-            outline: none;
-            background-color: #f5f8fa;
-            padding: 0 20px 40px 40px;
-          }
-        `}</style>
-      </div>
+        </Layout>
+      </>
     )
   }
 }
-
-export default DocsApp
