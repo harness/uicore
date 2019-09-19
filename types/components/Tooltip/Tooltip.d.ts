78d4c5d00dfa9eefa8d207c59e10f6f39d77068f
Tan Nhu <tannhu@gmail.com>
2019-09-19 08:07:34 -0700

    Clean up

--- types/components/Tooltip/Tooltip.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-import { ITooltipProps } from '@blueprintjs/core';
-export default function (props: ITooltipProps): JSX.Element;
--- types/core/Utils.d.ts
+++
@@ -1,6 +0,0 @@
-import { MouseEvent } from 'react';
-declare function stopEvent(event: MouseEvent): void;
-declare const _default: {
-    stopEvent: typeof stopEvent;
-};
-export default _default;
