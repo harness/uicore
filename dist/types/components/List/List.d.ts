9c1683379bf55a2635ff4ae6b4ed4e082ed9e986
Tan Nhu <tan@harness.io>
2019-10-10 10:31:59 -0700

    Clean up

--- dist/types/components/List/List.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface Props extends React.HTMLProps<HTMLElement> {
-    /** If true, render List as an ordered list, default is false */
-    ordered?: boolean;
-    /** List children */
-    children: React.ReactNode;
-}
-declare function List(props: Props): JSX.Element;
-export { List };
