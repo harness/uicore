9dafffa9aff32f4493187e1c068b4e5dba13925e
Tan Nhu <tan@harness.io>
2019-10-11 14:06:36 -0700

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,14 +5,14 @@ export NEXT_TELEMETRY_DISABLED=1
 export VERSION=$(awk '/version/{gsub(/("|",)/,"",$2);print $2};' package.json)
 
 # Create a new branch like v1.0.1
-git checkout -b v$NEW_VERSION
+git checkout -b v$VERSION
 
 # Build
 yarn build
 
 # Force adding ./dist into git because it's in .gitignore
 git add -f ./dist/*
-git commit . -m"Release version: v$NEW_VERSION"
+git commit . -m"Release version: v$VERSION"
 
 # mkdir -p /jenkins-storage/ui/uikit/$NEW_VERSION
 # cp -R ./* /jenkins-storage/ui/uikit/$NEW_VERSION
