bed57b5df18500cd48efab68da6e74ae9e82ebd8
Tan Nhu <tan@harness.io>
2019-10-11 12:52:16 -0700

    Add release scripts

---
+++ scripts/release-watcher.sh
@@ -0,0 +1,17 @@
+#
+# This script is used in Jenkins' uikit-watcher to watch for git
+# pushes. If a version change happens in package.json and there is
+# no release for it, the job triggers a downstream job (uikit-release)
+# to build a new release.
+#
+# When this script exits with status 1, the downstream job is not triggered.
+
+VERSION=$(awk '/version/{gsub(/("|",)/,"",$2);print $2};' package.json)
+RELEASE_EXISTS=$(git branch -r | grep "origin/release/$VERSION")
+
+if [ "$RELEASE_EXISTS" ]; then
+  echo "Release exists. Doing nothing."
+  exit 1
+else
+  echo "Trigger new release build for version $VERSION..."
+fi
---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+# Disable NextJS tracking
+export NEXT_TELEMETRY_DISABLED=1
+export VERSION=$(awk '/version/{gsub(/("|",)/,"",$2);print $2};' package.json)
+
+# Create a new branch like v1.0.1
+git checkout -b v$NEW_VERSION
+
+# Build
+yarn build
+
+# Force adding ./dist into git because it's in .gitignore
+git add -f ./dist/*
+git commit . -m"Release version: v$NEW_VERSION"
+
+# mkdir -p /jenkins-storage/ui/uikit/$NEW_VERSION
+# cp -R ./* /jenkins-storage/ui/uikit/$NEW_VERSION
+
+echo "VERSION=v$VERSION" > build.propertiess
