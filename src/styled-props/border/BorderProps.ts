bf0eeb4ebeb594b4b54699d621fed1f250b812e5
Tan Nhu <tan@harness.io>
2019-10-21 15:23:27 -0700

    Move border props to a separate folder

---
+++ src/styled-props/border/BorderProps.ts
@@ -0,0 +1,11 @@
+import { Color } from 'core/Color';
+
+export interface BorderProps {
+  top?: boolean
+  right?: boolean
+  bottom?: boolean
+  left?: boolean
+  color?: Color
+
+  /* TODO: Support border-width, border-style, border-radius, etc... */
+}
