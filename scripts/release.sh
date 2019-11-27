85fe6563824e2406c154e5a9b7f171075b9dea8e
Tan Nhu <tan@harness.io>
2019-11-26 16:01:59 -0800

    Add setup command to make local development easier

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,8 +7,10 @@ export VERSION=$(awk '/version/{gsub(/("|",)/,"",$2);print $2};' package.json)
 # Exit when any command fails
 set -e
 
-# GITHUB_HARNESS_BOT_TOKEN is secret from Jenkins
-echo $GITHUB_HARNESS_BOT_TOKEN > ~/.npmrc
+echo @wings-software:registry=https://npm.pkg.github.com > ~/.npmrc
+# GITHUB_HARNESS_BOT_TOKEN is a secret line from Jenkins (npm.pkg.github.com/:_authToken=XXX)
+echo $GITHUB_HARNESS_BOT_TOKEN >> ~/.npmrc
+echo always-auth=true >> ~/.npmrc
 
 # Create a new branch like v1.0.1
 git checkout -b v$VERSION
