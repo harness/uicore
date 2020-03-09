caf49fca527296010285e5803f6f7d477e9ff016
Rachit Aggarwal <rachit.aggarwal@harness.io>
2020-03-09 14:18:35 +0530

    chore: [PL-8999]: Adding tests for multi select component

---
+++ src/components/MultiSelect/MultiSelect.test.tsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import { render, fireEvent, act, wait } from '@testing-library/react'
+
+import { MultiSelect, MultiSelectOption } from './MultiSelect'
+
+import data from '../../../docs/shared/pokedex.json'
+
+const items: MultiSelectOption[] = data
+  .map(row => ({
+    label: row.name,
+    value: row.id
+  }))
+  .slice(0, 20)
+
+describe('<MultiSelect/> tests', () => {
+  beforeAll(() => {
+    jest.useFakeTimers()
+  })
+
+  test('works with static data', () => {
+    const { container } = render(<MultiSelect items={items} allowCreatingNewItems={true} />)
+    expect(container).toMatchSnapshot()
+    const input = container.querySelector('.bp3-input-ghost')!
+    expect(input).toBeDefined()
+    fireEvent.keyDown(input, { key: 'Enter', code: 13 })
+    expect(container.querySelectorAll('li.menuItem').length).toBe(items.length)
+  })
+
+  test('works with async data', async () => {
+    const TIMEOUT = 5000
+    const asyncData = () => {
+      return new Promise<MultiSelectOption[]>(resolve => {
+        setTimeout(() => {
+          resolve(items)
+        }, TIMEOUT)
+      })
+    }
+    const { container, queryByText } = render(<MultiSelect items={asyncData} />)
+    expect(container).toMatchSnapshot()
+
+    const input = container.querySelector('.bp3-input-ghost')!
+
+    expect(input).toBeDefined()
+    fireEvent.focus(input)
+
+    expect(container).toMatchSnapshot('Loading state')
+    expect(queryByText('Loading results...')).toBeDefined()
+
+    await act(() => {
+      jest.runTimersToTime(TIMEOUT)
+
+      return wait()
+    })
+
+    expect(container).toMatchSnapshot('Final state')
+    fireEvent.keyDown(input, { key: 'Enter', code: 13 })
+    expect(container.querySelectorAll('li.menuItem').length).toBe(items.length)
+  })
+
+  test('works with filtering', () => {
+    const { container } = render(<MultiSelect items={items} allowCreatingNewItems={true} />)
+    const input = container.querySelector('.bp3-input-ghost')!
+    fireEvent.focus(input)
+
+    fireEvent.change(input, { target: { value: 'cha' } })
+
+    expect(container).toMatchSnapshot('Filtered Menu')
+    expect(container.querySelectorAll('li.menuItem').length).toBe(3)
+  })
+
+  test('should work when no matching results found', () => {
+    const { container, queryByText } = render(<MultiSelect items={items} />)
+    const input = container.querySelector('.bp3-input-ghost')!
+
+    fireEvent.focus(input)
+
+    fireEvent.change(input, { target: { value: 'junk' } })
+
+    expect(container).toMatchSnapshot('Filtered Menu')
+    expect(queryByText('No matching results found')).toBeDefined()
+  })
+
+  test('should work when new item is created', () => {
+    const { container } = render(<MultiSelect items={items} allowCreatingNewItems={true} />)
+    const input = container.querySelector('.bp3-input-ghost')!
+
+    fireEvent.focus(input)
+
+    fireEvent.change(input, { target: { value: 'newItem' } })
+    expect(container.querySelectorAll('.bp3-button-text').length).toBe(1)
+    expect(container.querySelectorAll('.bp3-button-text')[0].textContent).toBe('newItem')
+  })
+
+  test(' works on Change', () => {
+    const onChange = jest.fn()
+    const { container, queryByText } = render(<MultiSelect items={items} onChange={onChange} />)
+    const input = container.querySelector('.bp3-input-ghost')!
+
+    fireEvent.focus(input)
+    fireEvent.change(input, { target: { value: 'char' } })
+
+    const pokemon = [items.find(item => item.label === 'Charizard')]
+    const opt = queryByText(pokemon[0]!.label)
+    expect(opt).toBeDefined()
+
+    if (opt) {
+      fireEvent.click(opt)
+      expect(onChange).toHaveBeenCalledWith(pokemon)
+    }
+  })
+})
