3f238f691e28cb75309003004cddf2f2e1955b5b
Tan Nhu <tan@harness.io>
2020-05-14 15:28:09 -0700

    [CDNG-5]: Fix typings

--- src/components/TagInput/SimpleTagInput.tsx
+++ src/components/TagInput/SimpleTagInput.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react'
 import { TagInput, TagInputProps } from './TagInput'
 
-export function SimpleTagInput(props: TagInputProps<string>) {
+export function SimpleTagInput(props: Omit<TagInputProps<string>, 'labelFor' | 'keyOf'>) {
   const labelFor = useCallback((item: string) => item, [])
   const itemFromNewTag = useCallback((newTag: string) => newTag, [])
 
