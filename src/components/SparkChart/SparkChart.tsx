a0fa6a81d6eb4e34eb059196741994836058ffcf
Shashanka Panuganti <shashanka.panuganti@harness.io>
2020-05-24 10:29:09 -0700

    feat: [CDNG-6]: add custom props for spark chart

--- src/components/SparkChart/SparkChart.tsx
+++ src/components/SparkChart/SparkChart.tsx
@@ -5,9 +5,7 @@ import { Container } from '../Container/Container'
 import { Color } from '../../core/Color'
 import { Utils } from '../../core/Utils'
 
-
 export interface SparkChartProps {
-
   /** numerical data array to pass */
   data: number[]
 
@@ -24,19 +22,18 @@ export interface SparkChartProps {
 export const SparkChart: React.FC<SparkChartProps> = ({
   data,
   className = '',
-  onClick = () => { },
+  onClick = () => {},
   color = Color.BLUE_450
 }) => {
-
   const max = Math.max(...data)
   const bgColor = Utils.getRealCSSColor(color)
   return (
-    <Container role='spark-chart' className={css.main} onClick={onClick}>
+    <Container role="spark-chart" className={css.main} onClick={onClick}>
       <div className="container vertical rounded">
         {data.map((item, index) => {
-          const percent = (item / max) * 100;
+          const percent = (item / max) * 100
           return (
-            <div role='spark' className={cx(`progress-bar`, className)} key={index} >
+            <div role="spark" className={cx(`progress-bar`, className)} key={index}>
               <div className={`progress-track`}>
                 <div className={`progress-fill`} style={{ backgroundColor: bgColor, height: `${percent}%` }}></div>
               </div>
@@ -44,6 +41,6 @@ export const SparkChart: React.FC<SparkChartProps> = ({
           )
         })}
       </div>
-    </Container >
+    </Container>
   )
 }
