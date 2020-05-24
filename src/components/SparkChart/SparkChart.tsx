05dbbddb2e9be21a3503bc938623165ef5f8a54e
Shashanka Panuganti <shashanka.panuganti@harness.io>
2020-05-24 10:23:25 -0700

    feat: [CDNG-6]: add custom props for spark chart

--- src/components/SparkChart/SparkChart.tsx
+++ src/components/SparkChart/SparkChart.tsx
@@ -1,32 +1,49 @@
-import React from 'react'
-import css from './SparkChart.css';
+import React, { FormEvent } from 'react'
+import css from './SparkChart.css'
+import cx from 'classnames'
 import { Container } from '../Container/Container'
+import { Color } from '../../core/Color'
+import { Utils } from '../../core/Utils'
 
 
 export interface SparkChartProps {
+
+  /** numerical data array to pass */
   data: number[]
+
+  /** className to be appended to default className */
+  className?: string
+
+  /** onClick Event */
+  onClick?: (event: FormEvent<HTMLElement>) => void
+
+  /** custom fill color */
+  color?: Color
 }
 
 export const SparkChart: React.FC<SparkChartProps> = ({
-  data
+  data,
+  className = '',
+  onClick = () => { },
+  color = Color.BLUE_450
 }) => {
 
   const max = Math.max(...data)
-
+  const bgColor = Utils.getRealCSSColor(color)
   return (
-    <Container className={css.main}>
+    <Container role='spark-chart' className={css.main} onClick={onClick}>
       <div className="container vertical rounded">
         {data.map((item, index) => {
           const percent = (item / max) * 100;
           return (
-            <div className={`progress-bar`} key={index}>
+            <div role='spark' className={cx(`progress-bar`, className)} key={index} >
               <div className={`progress-track`}>
-                <div className={`progress-fill`} style={{ height: `${percent}%` }}></div>
+                <div className={`progress-fill`} style={{ backgroundColor: bgColor, height: `${percent}%` }}></div>
               </div>
             </div>
           )
         })}
       </div>
-    </Container>
+    </Container >
   )
 }
--- src/components/SparkChart/__tests__/SparkChart.test.tsx
+++ src/components/SparkChart/__tests__/SparkChart.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { SparkChart } from '../SparkChart'
+import { Color } from 'core/Color';
 
 
 describe('SparkChrt unit tests', () => {
@@ -10,6 +11,17 @@ describe('SparkChrt unit tests', () => {
     );
 
     expect(container.querySelectorAll('.progress-bar').length).toBe(7)
+  })
+
+  test('render spark chart with custom props', () => {
+    const mockOnClick = jest.fn(() => { })
+    const { container, getByRole } = render(
+      <SparkChart data={[2, 3, 4, 1, 5, 2, 5]} onClick={mockOnClick} color={Color.BLACK} className={'custom'} />
+    );
+
+    expect(container.querySelectorAll('.progress-bar').length).toBe(7)
+    fireEvent.click(getByRole('spark-chart'));
+    expect(mockOnClick).toBeCalled();
   })
 
 });
