b4ce13ccf61b9e9cba640b2e55a6f2813af4abc7
Rachit Aggarwal <rachit.aggarwal@harness.io>
2020-05-25 17:19:46 +0530

    feat: [CVNG-21]: addressing comments

--- src/components/ColumnChart/ColumnChart.ts
+++ src/components/ColumnChart/ColumnChart.ts
@@ -1,4 +1,4 @@
-const defaultConfig = {
+export const columnChartDefaultConfig = {
   chart: {
     type: 'column',
     width: 400,
@@ -15,7 +15,7 @@ const defaultConfig = {
   series: [
     {
       name: '',
-      data: [71.5, 106.4, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+      data: [],
       showInLegend: false
     }
   ],
@@ -23,9 +23,3 @@ const defaultConfig = {
     enabled: false
   }
 }
-
-const getColumnChartDefaultConfig = () => {
-  return defaultConfig
-}
-
-export { getColumnChartDefaultConfig }
--- src/index.ts
+++ src/index.ts
@@ -68,5 +68,5 @@ export {
 } from './components/SelectWithSubview/SelectWithSubview'
 export { SparkChart } from './components/SparkChart/SparkChart'
 export { ModalExample } from './components/ModalExample/ModalExample'
-export { getTimeSeriesChartDefaultConfig } from './components/TimeSeriesChart/TimeSeriesChart'
-export { getColumnChartDefaultConfig } from './components/ColumnChart/ColumnChart'
+export { timeSeriesChartDefaultConfig } from './components/TimeSeriesChart/TimeSeriesChart'
+export { columnChartDefaultConfig } from './components/ColumnChart/ColumnChart'
