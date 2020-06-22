b8bc76652468c00809123b303129541f2460f229
Kapil Kumawat <kapil.kumawat@harness.io>
2020-06-22 18:27:59 +0530

    fix: RadioGroup formik integration (#207)
    * fix: RadioGroup formik integration
    
    * fix: RadioGroup formik integration

--- src/components/FormikForm/FormikForm.tsx
+++ src/components/FormikForm/FormikForm.tsx
@@ -7,7 +7,7 @@ import {
   MultiSelectProps as UiKitMultiSelectProps
 } from '../MultiSelect/MultiSelect'
 import { Checkbox as UiKitCheckbox, CheckboxProps as UiKitCheckboxProps } from '../Checkbox/Checkbox'
-import { Radio } from '../Radio/Radio'
+import cssRadio from '../Radio/Radio.css'
 import { TagInputProps as UiKitTagInputProps, TagInput as UiKitTagInput } from '../TagInput/TagInput'
 import checkBoxCss from '../Checkbox/Checkbox.css'
 import {
@@ -184,6 +184,14 @@ const RadioGroup = (props: RadioGroupProps & FormikContenxtProps<any>) => {
     onChange,
     ...rest
   } = props
+
+  const itemTemp = items.map(item => {
+    const { className = '' } = item
+    return {
+      ...item,
+      className: cx(cssRadio.radio, className)
+    }
+  })
   return (
     <FormGroup labelFor={name} helperText={helperText} intent={intent} disabled={disabled} inline={inline} {...rest}>
       <BpRadioGroup
@@ -191,14 +199,12 @@ const RadioGroup = (props: RadioGroupProps & FormikContenxtProps<any>) => {
         name={name}
         disabled={disabled}
         selectedValue={get(formik?.values, name)}
+        options={itemTemp}
         onChange={(e: React.FormEvent<HTMLInputElement>) => {
           formik?.setFieldValue(name, e.currentTarget.value)
           onChange?.(e)
-        }}>
-        {items.map(radio => (
-          <Radio key={radio.value} {...radio} disabled={disabled} />
-        ))}
-      </BpRadioGroup>
+        }}
+      />
     </FormGroup>
   )
 }
