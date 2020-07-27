4753df307509a9322a3e9bb802d13539c0c1b50d
petricfilip <filip.petric@harness.io>
2020-07-27 14:39:23 +0200

    feat: [CE-1939]: update snapshot

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -4,15 +4,15 @@ import { Avatar } from './Avatar'
 
 describe('Render basic component', () => {
   test('should check snapshot with name', () => {
-    const { container } = render(<Avatar name="John Doe" />)
+    const { container } = render(<Avatar color="red" name="John Doe" />)
     expect(container).toMatchSnapshot()
   })
   test('should check snapshot with email', () => {
-    const { container } = render(<Avatar email="John.Doe@harness.io" />)
+    const { container } = render(<Avatar color="red" email="John.Doe@harness.io" />)
     expect(container).toMatchSnapshot()
   })
   test('should check snapshot with image', () => {
-    const { container } = render(<Avatar src="https://www.mangaship.com/Content/img/human-avatar.png" />)
+    const { container } = render(<Avatar color="red" src="https://www.mangaship.com/Content/img/human-avatar.png" />)
     expect(container).toMatchSnapshot()
   })
 })
