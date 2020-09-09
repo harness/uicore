45a162af5f4fcec5f1e0b7e5019ffe2a1bf1dac1
Michael Tran <michael.tran@harness.io>
2020-09-09 16:57:24 -0700

    feat: [CDC-9322]: address comments

--- src/components/TagInput/SimpleTagInput.tsx
+++ src/components/TagInput/SimpleTagInput.tsx
@@ -2,7 +2,10 @@ import React, { useCallback } from 'react'
 import { TagInput, TagInputProps } from './TagInput'
 
 export function SimpleTagInput(props: Omit<TagInputProps<string>, 'labelFor' | 'keyOf' | 'itemFromNewTag'>) {
-  const labelFor = useCallback((item: string | any) => (typeof item === 'string' ? item : item.value), [])
+  const labelFor = useCallback(
+    (item: string | { label: string; value: string }) => (typeof item === 'string' ? item : item.value),
+    []
+  )
   const itemFromNewTag = useCallback((newTag: string) => newTag, [])
 
   return (
