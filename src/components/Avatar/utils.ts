9b2eeade6fd7c169f88b562285ca670f528c2fd9
Swaraj Cheguri <swarajcheguri@Swaraj-Cheguri-MBP.local>
2020-11-18 19:46:19 +0530

    fix: [CE-1939]: lint errors fix

--- src/components/Avatar/utils.ts
+++ src/components/Avatar/utils.ts
@@ -1,4 +1,4 @@
-export const getInitialsFromNameOrEmail = (name: string = '', email: string = '') => {
+export const getInitialsFromNameOrEmail = (name = '', email = '') => {
   let initialsFromName = name
     .split(/-| /)
     .map((n: string) => n[0])
