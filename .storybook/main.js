f45b53f01d1ff9b4136674cb2c6714b117b3df65
Vivek Kumar Bansal <vivek.bansal@harness.io>
2020-12-23 16:07:36 +0530

    chore: add storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,43 @@
 const path = require('path')
 const webpackConfig = require('../webpack.config')
 
+const srcFolder = path.resolve(__dirname, '../src')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
-  // webpackFinal: config => {
-  //   return {
-  //     ...config,
-  //     module: {
-  //       ...config.module,
-  //       rules: [
-  //         ...webpackConfig.module.rules,
-  //         // .map((rule) => {
-  //         //   return {
-  //         //     ...rule,
-  //         //     include: path.resolve(process.cwd(), 'src')
-  //         //   }
-  //         // }),
-  //         ...config.module.rules
-  //       ]
-  //     },
-  //     plugins: [
-  //       ...config.plugins,
-  //       ...webpackConfig.plugins
-  //     ],
-  //     stats: 'minimal'
-  //   }
-  // }
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: config => {
+    const fileLoaderRule = config.module.rules.find(rule => rule.test.test('.svg'))
+    fileLoaderRule.exclude = srcFolder
+
+    const CSSLoader = config.module.rules.find(rule => rule.test.test('.css'))
+    CSSLoader.exclude = srcFolder
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      include: srcFolder,
+      use: ['@svgr/webpack']
+    })
+
+    config.module.rules.push({
+      test: /\.css$/,
+      include: srcFolder,
+      use: [
+        { loader: 'style-loader' },
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+            modules: {
+              mode: 'local',
+              localIdentName: '[name]--[local]'
+            }
+          }
+        },
+        { loader: 'postcss-loader' }
+      ]
+    })
+
+    return config
+  }
 }
