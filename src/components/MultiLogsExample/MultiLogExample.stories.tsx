20e7536c468cad129c5060ccc322363e485e8224
srdjan-srdjan <69592850+srdjan-srdjan@users.noreply.github.com>
2021-01-16 13:06:52 +0100

    feat: Auto fit logs to parent; fix low performance; (#42)

--- src/components/MultiLogsExample/MultiLogExample.stories.tsx
+++ src/components/MultiLogsExample/MultiLogExample.stories.tsx
@@ -5,7 +5,15 @@ import { MultiLogsExample } from './MultiLogsExample'
 
 export default {
   title: 'Components / MultiLogsExample',
-  component: MultiLogsExample
+  component: MultiLogsExample,
+  parameters: {
+    controls: {
+      disabled: true
+    },
+    actions: {
+      disabled: true
+    }
+  }
 } as Meta
 
 export const Basic: Story<void> = _args => <MultiLogsExample />
--- src/components/MultiLogsExample/MultiLogsExample.tsx
+++ src/components/MultiLogsExample/MultiLogsExample.tsx
@@ -4,6 +4,7 @@ import { MultiLogsViewer } from '../MultiLogsViewer/MultiLogsViewer'
 import { Text } from '../Text/Text'
 import { Color } from '../../core/Color'
 import { ExpandingSearchInput } from '../ExpandingSearchInput/ExpandingSearchInput'
+import css from './MultiLogExample.css'
 
 export const MultiLogsExample: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('')
@@ -23,6 +24,9 @@ export const MultiLogsExample: React.FC = () => {
   ])
   const [loadingIndex, setLoadingIndex] = useState<number>(-1)
 
+  // util for testing
+  const [containerHeight, setContainerHeight] = useState(300)
+
   const toggleSection = (sectionNo: number) => {
     panelArr[sectionNo] = !panelArr[sectionNo]
     setPanelArr([...panelArr])
@@ -40,9 +44,12 @@ export const MultiLogsExample: React.FC = () => {
     return logsArr[sectionIdx]
   }
 
-  const addLogsToSection = (sectionIdx: number) => {
-    const line = Math.random().toString()
-    logsArr[sectionIdx] += `\n${line}`
+  const addLogsToSection = (sectionIdx: number, rows = 1, clear = false) => {
+    if (clear) logsArr[sectionIdx] = ''
+    for (let i = 0; i < rows; i++) {
+      const line = Math.random().toString()
+      logsArr[sectionIdx] += `\n${line}`
+    }
     setLogsArr([...logsArr])
   }
 
@@ -77,7 +84,21 @@ export const MultiLogsExample: React.FC = () => {
         <button onClick={() => clearSection(3)}> 3 </button>
       </div>
       <div>
-        Add to sectio:
+        Populate section x 10000:
+        <button onClick={() => addLogsToSection(0, 5000, true)}> 0 </button>
+        <button onClick={() => addLogsToSection(1, 5000, true)}> 1 </button>
+        <button onClick={() => addLogsToSection(2, 5000, true)}> 2 </button>
+        <button onClick={() => addLogsToSection(3, 5000, true)}> 3 </button>
+      </div>
+      <div>
+        Add to section x 5000:
+        <button onClick={() => addLogsToSection(0, 5000)}> 0 </button>
+        <button onClick={() => addLogsToSection(1, 5000)}> 1 </button>
+        <button onClick={() => addLogsToSection(2, 5000)}> 2 </button>
+        <button onClick={() => addLogsToSection(3, 5000)}> 3 </button>
+      </div>
+      <div>
+        Add to section:
         <button onClick={() => addLogsToSection(0)}> 0 </button>
         <button onClick={() => addLogsToSection(1)}> 1 </button>
         <button onClick={() => addLogsToSection(2)}> 2 </button>
@@ -98,7 +119,11 @@ export const MultiLogsExample: React.FC = () => {
         <button onClick={() => cloneLogsArr()}>Clone content</button>
         <button onClick={() => replaceText(0)}>Replace content section 0</button>
       </div>
-
+      <div>
+        Set container Height
+        <button onClick={() => setContainerHeight(containerHeight + 50)}>+50px</button>
+        <button onClick={() => setContainerHeight(containerHeight - 50)}>-50px</button>
+      </div>
       <ExpandingSearchInput
         onChange={ev => {
           setSearchTerm(ev)
@@ -118,53 +143,58 @@ export const MultiLogsExample: React.FC = () => {
         }}>
         Prev
       </button>
-      <MultiLogsViewer
-        rows={10}
-        loadingIndex={loadingIndex}
-        numberOfLogSections={4}
-        titleForSection={sectionIndex => {
-          switch (sectionIndex) {
-            case 0:
-              return 'Set up job'
-            case 1:
-              return 'Run actions/checkout@v2'
-            case 2:
-              return 'Run make'
-            case 3:
-              return 'Complete job'
-          }
-        }}
-        sectionArr={panelArr}
-        activePanel={openedIndex}
-        isSectionOpen={(sectionIndex: number) => panelArr[sectionIndex]}
-        rightElementForSection={() => {
-          return <Text color={Color.GREY_100}>2m 38s</Text>
-        }}
-        logContentForSection={logContentForSection}
-        searchDir={searchDir}
-        highlightedIndex={highlightInd}
-        searchText={searchTerm}
-        toggleSection={(index: number) => {
-          panelArr[index] = !panelArr[index]
-          setPanelArr([...panelArr])
-        }}
-        updateSection={(currentIndex: number, nextIndex = -1) => {
-          if (nextIndex > -1) {
-            panelArr[currentIndex] = false
-            panelArr[nextIndex] = true
-            setOpenedIndex(nextIndex)
-          } else {
-            panelArr[currentIndex] = !panelArr[currentIndex]
-            if (!panelArr[currentIndex] && currentIndex === 0) {
-              setOpenedIndex(-1)
-            } else {
-              setOpenedIndex(currentIndex)
+      <div>
+        <MultiLogsViewer
+          minRows={5}
+          style={{ height: `${containerHeight}px`, overflowY: 'scroll' }}
+          className={css.multiLogsViewer}
+          loadingIndex={loadingIndex}
+          numberOfLogSections={4}
+          titleForSection={sectionIndex => {
+            switch (sectionIndex) {
+              case 0:
+                return 'Set up job'
+              case 1:
+                return 'Run actions/checkout@v2'
+              case 2:
+                return 'Run make'
+              case 3:
+                return 'Complete job'
             }
-          }
+          }}
+          scrollbackLines={1000000}
+          sectionArr={panelArr}
+          activePanel={openedIndex}
+          isSectionOpen={(sectionIndex: number) => panelArr[sectionIndex]}
+          rightElementForSection={() => {
+            return <Text color={Color.GREY_100}>2m 38s</Text>
+          }}
+          logContentForSection={logContentForSection}
+          searchDir={searchDir}
+          highlightedIndex={highlightInd}
+          searchText={searchTerm}
+          toggleSection={(index: number) => {
+            panelArr[index] = !panelArr[index]
+            setPanelArr([...panelArr])
+          }}
+          updateSection={(currentIndex: number, nextIndex = -1) => {
+            if (nextIndex > -1) {
+              panelArr[currentIndex] = false
+              panelArr[nextIndex] = true
+              setOpenedIndex(nextIndex)
+            } else {
+              panelArr[currentIndex] = !panelArr[currentIndex]
+              if (!panelArr[currentIndex] && currentIndex === 0) {
+                setOpenedIndex(-1)
+              } else {
+                setOpenedIndex(currentIndex)
+              }
+            }
 
-          setPanelArr([...panelArr])
-        }}
-      />
+            setPanelArr([...panelArr])
+          }}
+        />
+      </div>
     </>
   )
 }
