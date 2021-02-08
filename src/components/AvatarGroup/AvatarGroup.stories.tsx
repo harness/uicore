11e90ac65c03b1f5eca998d7d54ea8722d4f6df5
Swaraj Cheguri <swarajcheguri@Swaraj-Cheguri-MBP.local>
2021-02-08 17:39:56 +0530

    feat: [CDNG-3175]: with more story pages

--- src/components/AvatarGroup/AvatarGroup.stories.tsx
+++ src/components/AvatarGroup/AvatarGroup.stories.tsx
@@ -7,91 +7,143 @@ export default {
   title: 'Components / AvatarGroup',
   component: AvatarGroup
 } as Meta
-
-export const Basic: Story<AvatarGroupProps> = args => (
-  <Layout.Horizontal spacing="small" id="primary-buttons">
-    <AvatarGroup {...args} />
-    <AvatarGroup
-      onAdd={() => {
-        // eslint-disable-next-line
-        alert('add button clicked')
-      }}
-      avatars={[
-        { name: 'Aahn Boe SS' },
-        { email: 'CohnDoe.com.HD@harnes.io' },
-        { name: 'EahnFoe' },
-        { name: 'Gahn Hoe', backgroundColor: Color.RED_800 },
-        { name: '' },
-        { src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg' }
-      ]}
-      size="normal"
-    />
-    <AvatarGroup
-      onAdd={() => {
-        // eslint-disable-next-line
-        alert('add button clicked')
-      }}
-      avatarGroupProps={{
-        color: Color.BLACK,
-        hoverCard: false
-      }}
-      avatars={[
-        { name: 'Aahn Boe SS' },
-        { email: 'CohnDoe.com.HD@harnes.io' },
-        { name: 'EahnFoe' },
-        { name: 'Gahn Hoe' },
-        { name: '' },
-        { src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg' }
-      ]}
-      size="small"
-    />
-    <AvatarGroup
-      avatars={[
-        { name: 'Aahn Boe SS' },
-        { email: 'CohnDoe.com.HD@harnes.io' },
-        { name: 'EahnFoe' },
-        { name: 'Gahn Hoe' },
-        { src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg' }
-      ]}
-      size="medium"
-      onAdd={() => {
-        // eslint-disable-next-line
-        alert('add button clicked')
-      }}
-    />
-    <AvatarGroup
-      avatars={[
-        { name: 'Aahn Boe SS' },
-        {
-          src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg'
-        },
-        { email: 'CohnDoe.com.HD@harnes.io' },
-        { name: 'EahnFoe' },
-        {
-          src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg'
-        },
-        { name: 'Gahn Hoe' },
-        { name: 'Gahn fdHoe' }
-      ]}
-      size="xsmall"
-      onAdd={() => {
-        // eslint-disable-next-line
-        alert('add button clicked')
-      }}
-    />
-    <AvatarGroup
-      avatars={[
-        { name: 'Aahn Boe SS' },
-        { email: 'CohnDoe.com.HD@harnes.io' },
-        { name: 'EahnFoe' },
-        { name: 'Gahn Hoe' },
-        { src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg' }
-      ]}
-      size="large"
-      onAdd={() => {
-        // eslint-disable-next-line
-        alert('add button clicked')
-      }}
-    />
-  </Layout.Horizontal>
-)
+export const Normal: Story<AvatarGroupProps> = args => {
+  let { avatars } = args
+  if (!avatars) {
+    avatars = [
+      { name: 'Aahn Boe SS' },
+      { email: 'CohnDoe.com.HD@harnes.io' },
+      { name: 'EahnFoe' },
+      { name: 'Gahn Hoe', backgroundColor: Color.RED_800 },
+      { name: '' },
+      {
+        src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg'
+      }
+    ]
+  }
+  return (
+    <Layout.Horizontal spacing="small" id="primary-buttons">
+      <AvatarGroup
+        onAdd={() => {
+          // eslint-disable-next-line
+          alert('add button clicked')
+        }}
+        {...args}
+        size="normal"
+        avatars={avatars}
+      />
+    </Layout.Horizontal>
+  )
+}
+export const Xsmall: Story<AvatarGroupProps> = args => {
+  let { avatars } = args
+  if (!avatars) {
+    avatars = [
+      { name: 'Aahn Boe SS' },
+      { email: 'CohnDoe.com.HD@harnes.io' },
+      { name: 'EahnFoe' },
+      { name: 'Gahn Hoe', backgroundColor: Color.RED_800 },
+      { name: '' },
+      {
+        src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg'
+      }
+    ]
+  }
+  return (
+    <Layout.Horizontal spacing="small" id="primary-buttons">
+      <AvatarGroup
+        onAdd={() => {
+          // eslint-disable-next-line
+          alert('add button clicked')
+        }}
+        {...args}
+        size="xsmall"
+        avatars={avatars}
+      />
+    </Layout.Horizontal>
+  )
+}
+export const Small: Story<AvatarGroupProps> = args => {
+  let { avatars } = args
+  if (!avatars) {
+    avatars = [
+      { name: 'Aahn Boe SS' },
+      { email: 'CohnDoe.com.HD@harnes.io' },
+      { name: 'EahnFoe' },
+      { name: 'Gahn Hoe', backgroundColor: Color.RED_800 },
+      { name: '' },
+      {
+        src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg'
+      }
+    ]
+  }
+  return (
+    <Layout.Horizontal spacing="small" id="primary-buttons">
+      <AvatarGroup
+        onAdd={() => {
+          // eslint-disable-next-line
+          alert('add button clicked')
+        }}
+        {...args}
+        size="small"
+        avatars={avatars}
+      />
+    </Layout.Horizontal>
+  )
+}
+export const Large: Story<AvatarGroupProps> = args => {
+  let { avatars } = args
+  if (!avatars) {
+    avatars = [
+      { name: 'Aahn Boe SS' },
+      { email: 'CohnDoe.com.HD@harnes.io' },
+      { name: 'EahnFoe' },
+      { name: 'Gahn Hoe', backgroundColor: Color.RED_800 },
+      { name: '' },
+      {
+        src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg'
+      }
+    ]
+  }
+  return (
+    <Layout.Horizontal spacing="small" id="primary-buttons">
+      <AvatarGroup
+        onAdd={() => {
+          // eslint-disable-next-line
+          alert('add button clicked')
+        }}
+        {...args}
+        size="large"
+        avatars={avatars}
+      />
+    </Layout.Horizontal>
+  )
+}
+export const Medium: Story<AvatarGroupProps> = args => {
+  let { avatars } = args
+  if (!avatars) {
+    avatars = [
+      { name: 'Aahn Boe SS' },
+      { email: 'CohnDoe.com.HD@harnes.io' },
+      { name: 'EahnFoe' },
+      { name: 'Gahn Hoe', backgroundColor: Color.RED_800 },
+      { name: '' },
+      {
+        src: 'https://image.shutterstock.com/image-photo/buddha-statue-hyderabadhyderabadindia-260nw-1366155431.jpg'
+      }
+    ]
+  }
+  return (
+    <Layout.Horizontal spacing="small" id="primary-buttons">
+      <AvatarGroup
+        onAdd={() => {
+          // eslint-disable-next-line
+          alert('add button clicked')
+        }}
+        {...args}
+        size="medium"
+        avatars={avatars}
+      />
+    </Layout.Horizontal>
+  )
+}
