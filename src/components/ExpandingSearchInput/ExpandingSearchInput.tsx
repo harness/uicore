66a3ff31a53473001f161bf3aad39dbebd19a54e
mattblr-harness <79698746+mattblr-harness@users.noreply.github.com>
2021-03-16 21:00:13 +0000

    fix: added in important on the flip css classes (#95)
    * fix: added in important on the flip css classes as the left wasn't being respected on fixedText
    
    * fix: merge conflicts

--- src/components/ExpandingSearchInput/ExpandingSearchInput.tsx
+++ src/components/ExpandingSearchInput/ExpandingSearchInput.tsx
@@ -151,6 +151,8 @@ export function ExpandingSearchInput(props: PropsInterface) {
 
   const cssBtnWrapper = `${css.btnWrapper} ${flip ? css.flipBtnWrapper : ''} `
 
+  const cssFixedText = `${css.fixedText} ${flip ? css.flipFixedText : ''}`
+
   // needs to be the last useEffect
   // using ref instead of state variable to avoid triggering a rerender
   useEffect(() => {
@@ -174,7 +176,7 @@ export function ExpandingSearchInput(props: PropsInterface) {
       />
       {value.length > 0 ? (
         <>
-          {fixedText ? <span className={css.fixedText}>{fixedText}</span> : null}
+          {fixedText ? <span className={cssFixedText}>{fixedText}</span> : null}
           <span className={cssBtnWrapper}>
             {showPrevNextButtons ? (
               <>
