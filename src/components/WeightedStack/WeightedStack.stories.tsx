546eb93eeffe9155044ff2d28fbd94537cfa7dd8
Vardan Bansal <vardan.bansal@harness.io>
2021-03-26 19:26:24 +0530

    feat: [CDNG-5425]: Adding documentation for WeightedStack component (#114)
    * feat: [CDNG-5425]: Added skeleton changes for WeightedStack
    
    * feat: [CDNG-5425]: Styling changes - part 1
    
    * feat: [CDNG-5425]: Fixed data interface
    
    * feat: [CDNG-5425]: Color integration
    
    * feat: [CDNG-5425]: Minor cleanup
    
    * feat: [CDNG-5425]: Stack label integration
    
    * feat: [CDNG-5425]: Styling changes  - part 2
    
    * feat: [CDNG-5425]: Styling changes  - part 3
    
    * feat: [CDNG-5425]: CSS changes
    
    * feat: [CDNG-5425]: CSS cleanup
    
    * feat: [CDNG-5425]: CSS changes
    
    * feat: [CDNG-5425]: Sorting data and updated test data
    
    * feat: [CDNG-5425]: Updated input data interface
    
    * feat: [CDNG-5425]: Fixed eslint issue
    
    * feat: [CDNG-5425]: Documentation - part 1
    
    * feat: [CDNG-5425]: Documentation complete

--- src/components/WeightedStack/WeightedStack.stories.tsx
+++ src/components/WeightedStack/WeightedStack.stories.tsx
@@ -1,12 +1,56 @@
 import React from 'react'
 import type { Meta, Story } from '@storybook/react'
+import { Title, Subtitle, ArgsTable, Stories, PRIMARY_STORY, Primary } from '@storybook/addon-docs/blocks'
 
 import { Color } from '../../core/Color'
+import { Text } from '../..'
 
 import { WeightedStack, WeightedStackProps } from './WeightedStack'
 
 export default {
-  title: 'Components / WeightedStack'
+  title: 'Components / WeightedStack',
+  component: WeightedStack,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      source: {
+        type: 'code'
+      },
+
+      page: function PageDescription() {
+        return (
+          <>
+            <Title>Weighted Stack</Title>
+            <Subtitle>
+              <pre>
+                <code>{`import {WeightedStack} from '@wings-software/uicore'`}</code>
+              </pre>
+            </Subtitle>
+            This component renders horizontally stacked bars. Bars are in a weighted-average proportion of their values.
+            <pre>
+              <code>
+                w<sub>i</sub> = x<sub>i</sub> / ∑ x<sub>i</sub>&nbsp;where x<sub>0</sub>, x<sub>1</sub>, x<sub>2</sub>
+                ... are weights(or values) of bars
+              </code>
+            </pre>
+            <Text style={{ fontWeight: 'bold' }}>Data Interface</Text>
+            <pre>
+              <code>
+                {`interface WeightedStackData {
+  label: string
+  value: number
+  color?: Color
+}`}
+              </code>
+            </pre>
+            <Primary />
+            <ArgsTable story={PRIMARY_STORY} />
+            <Stories />
+          </>
+        )
+      }
+    }
+  }
 } as Meta
 
 export const WeightedStackComp: Story<WeightedStackProps> = args => <WeightedStack {...args} />
