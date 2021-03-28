3d9494a9974302ee46a59467c4d49fc7ccefa0f5
Vardan Bansal <vardan.bansal@harness.io>
2021-03-29 00:05:28 +0530

    feat: [CDNG-5581]: highcharts initial config

--- src/components/Charts/AreaChart/AreaChart.tsx
+++ src/components/Charts/AreaChart/AreaChart.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts'
+
 import css from './AreaChart.css'
 
 export interface AreaChartProps {}
 
-export const AreaChart: React.FC<AreaChartProps> = () => {
-  return <></>
+export const AreaChart: React.FC<AreaChartProps> = _props => {
+  const options = {
+    title: {
+      text: 'My chart'
+    },
+    series: [
+      {
+        data: [1, 2, 3]
+      }
+    ]
+  }
+  return (
+    <div className={css.main}>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  )
 }
