596adef94a6c3e2197c7b64b3727e26efa149737
Swaraj Cheguri <swarajcheguri@Swaraj-Cheguri-MBP.local>
2021-03-31 19:20:50 +0530

    feat: [PL-13925] : with few more components

---
+++ src/components/Select/SelectV2.stories.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from 'react'
+import type { Meta, Story } from '@storybook/react'
+
+import { SelectV2, SelectV2Props, Layout, Button } from '../..'
+import { Title, Subtitle, ArgsTable, Stories, PRIMARY_STORY, Primary } from '@storybook/addon-docs/blocks'
+import data from '../../_stories/components/pokedex.json'
+import { omit } from 'lodash-es'
+export default {
+  title: 'Form / SelectV2',
+
+  component: SelectV2,
+  parameters: {
+    docs: {
+      source: {
+        type: 'code'
+      },
+
+      page: function PageDescription() {
+        return (
+          <>
+            <Title>SelectV2</Title>
+
+            <Subtitle>
+              <pre>
+                <code>{`import { SelectV2 }  from '@wings-software/uicore'`}</code>
+              </pre>
+            </Subtitle>
+            <Primary />
+            <ArgsTable story={PRIMARY_STORY} />
+
+            <Stories />
+          </>
+        )
+      }
+    }
+  },
+  decorators: [Story => <Story />]
+} as Meta
+
+interface SelectOption {
+  label: string
+  value: string | number | symbol
+  icon?: any
+}
+
+const itemsLocal: SelectOption[] = data.map(row => ({
+  label: row.name,
+  value: row.id,
+  icon: row.icon
+}))
+
+function dummyPromise(): Promise<SelectOption[]> {
+  return new Promise<SelectOption[]>(resolve => {
+    setTimeout(() => {
+      resolve(itemsLocal)
+    }, 5000)
+  })
+}
+
+export const SimpleSelect: Story<SelectV2Props> = args => {
+  const [val, setVal] = useState<SelectOption>()
+  const { items = itemsLocal } = args
+  const argsCopy = omit(args, ['items'])
+  return (
+    <Layout.Horizontal id="primary-borderless-buttons">
+      <SelectV2
+        items={items}
+        value={val}
+        onChange={setVal}
+        itemRenderer={(item, props) => (
+          <Button
+            style={{ width: '100%', display: 'block' }}
+            minimal
+            onClick={ev => {
+              props.handleClick(ev as any)
+            }}>
+            {item.label}
+          </Button>
+        )}
+        {...argsCopy}>
+        <Button
+          intent="primary"
+          minimal
+          style={{ border: '1px solid var(--grey-400)', width: '100%', display: 'block' }}
+          icon={val && val.icon && val.icon.name}
+          rightIcon="chevron-down"
+          text={val ? val.label : 'Search...'}
+        />
+      </SelectV2>
+    </Layout.Horizontal>
+  )
+}
+export const AsyncSelect: Story<SelectV2Props> = args => {
+  const { items = dummyPromise } = args
+  const argsCopy = omit(args, ['items'])
+  const [val, setVal] = useState<SelectOption>()
+  return (
+    <Layout.Horizontal id="primary-borderless-buttons">
+      <SelectV2
+        items={items}
+        value={val}
+        onChange={setVal}
+        itemRenderer={(item, props) => (
+          <Button style={{ width: '100%', display: 'block' }} minimal onClick={ev => props.handleClick(ev as any)}>
+            {item.label}
+          </Button>
+        )}
+        {...argsCopy}>
+        <Button
+          intent="primary"
+          minimal
+          style={{ border: '1px solid var(--grey-400)', width: '100%', display: 'block' }}
+          icon={val && val.icon && val.icon.name}
+          rightIcon="chevron-down"
+          text={val ? val.label : 'Search...'}
+        />
+      </SelectV2>
+    </Layout.Horizontal>
+  )
+}
