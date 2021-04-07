11dfef2a682b05ea19d68b6098f4321e59934ba1
jasmeet-harness <78347581+jasmeet-harness@users.noreply.github.com>
2021-04-07 11:50:44 +0530

    feat: [CDNG-5866]: handle orientation cases in WeightedStack (#141)
    * feat: [CDNG-5866]: handle orientation cases in WeightedStack
    
    * feat: [CDNG-5866]: override not required

--- src/components/WeightedStack/WeightedStack.tsx
+++ src/components/WeightedStack/WeightedStack.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import cx from 'classnames'
 
 import { Container } from '../Container/Container'
@@ -8,32 +8,71 @@ import { Text } from '../Text/Text'
 
 import css from './WeightedStack.css'
 
+export enum LabelPosition {
+  LEFT = 'LabelPosition.LEFT',
+  RIGHT = 'LabelPosition.RIGHT',
+  TOP = 'LabelPosition.TOP',
+  BOTTOM = 'LabelPosition.BOTTOM'
+}
 export interface WeightedStackData {
   label: string
   value: number
   color?: Color
 }
-export interface WeightedStackProps {
+export interface WeightedStackPropsCommon {
   data: WeightedStackData[]
   className?: string
 }
 
-export const WeightedStack: React.FC<WeightedStackProps> = ({ data = [], className }) => {
-  const sortedData = data.sort((point1: WeightedStackData, point2: WeightedStackData) =>
-    point1.value > point2.value ? -1 : point2.value > point1.value ? 1 : 0
+interface InlineLabelPosition extends WeightedStackPropsCommon {
+  labelPosition: LabelPosition.LEFT | LabelPosition.RIGHT
+  labelWidth: string
+}
+
+interface NonInlineLabelPosition extends WeightedStackPropsCommon {
+  labelPosition?: LabelPosition.TOP | LabelPosition.BOTTOM
+}
+
+export type WeightedStackProps = InlineLabelPosition | NonInlineLabelPosition
+
+export const WeightedStack: React.FC<WeightedStackProps> = props => {
+  const { data = [], className, labelPosition = LabelPosition.TOP } = props
+  const labelWidth = (props as InlineLabelPosition).labelWidth
+  const sortedData = useMemo(
+    () =>
+      data.sort((point1: WeightedStackData, point2: WeightedStackData) =>
+        point1.value > point2.value ? -1 : point2.value > point1.value ? 1 : 0
+      ),
+    [data]
   )
-  const max = Math.max(...sortedData.map((data: WeightedStackData) => data.value))
+  const maxValue = sortedData[0].value
+  const stackLabelWidthStyle =
+    labelPosition === LabelPosition.LEFT || labelPosition === LabelPosition.RIGHT
+      ? {
+          style: {
+            width: labelWidth
+          }
+        }
+      : {}
+
   return (
     <Container className={css.main}>
       {data.length ? (
         <div className={css.container}>
           {sortedData.map((item, index) => {
             const { label, value, color = Color.BLUE_450 } = item
-            const percent = (value / max) * 100
+            const percent = (value / maxValue) * 100
             const realColor = Utils.getRealCSSColor(color)
             return (
-              <div className={css.stack} key={index}>
-                <Text className={css.stackLabel}>
+              <div
+                className={cx(css.stack, {
+                  [css.stackRow]: labelPosition === LabelPosition.LEFT,
+                  [css.stackRowReverse]: labelPosition === LabelPosition.RIGHT,
+                  [css.stackColumn]: labelPosition === LabelPosition.TOP,
+                  [css.stackColumnReverse]: labelPosition === LabelPosition.BOTTOM
+                })}
+                key={index}>
+                <Text className={css.stackLabel} {...stackLabelWidthStyle}>
                   {label}&nbsp;({value})
                 </Text>
                 <div className={cx('progress-bar', className)}>
