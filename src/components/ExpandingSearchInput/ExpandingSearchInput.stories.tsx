5f893917dc3087a45c2a4f74a04e5ce622402b14
Vivek Kumar Bansal <5930351+vkbansal@users.noreply.github.com>
2021-06-29 16:28:11 +0530

    feat: add instances methods on expanding search input (#300)

--- src/components/ExpandingSearchInput/ExpandingSearchInput.stories.tsx
+++ src/components/ExpandingSearchInput/ExpandingSearchInput.stories.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import type { Meta, Story } from '@storybook/react'
 
-import { ExpandingSearchInput, PropsInterface } from './ExpandingSearchInput'
+import {
+  ExpandingSearchInputWithRef as ExpandingSearchInput,
+  ExpandingSearchInputProps,
+  ExpandingSearchInputHandle
+} from './ExpandingSearchInput'
 
 export default {
   title: 'Components/ExpandingSearchInput',
   component: ExpandingSearchInput
 } as Meta
 
-export const Basic: Story<PropsInterface> = args => <ExpandingSearchInput {...args} />
+export const Basic: Story<ExpandingSearchInputProps> = args => {
+  const ref = React.useRef<ExpandingSearchInputHandle>()
+
+  return (
+    <div>
+      <ExpandingSearchInput {...args} ref={ref} />
+      <br />
+      <br />
+      <button onClick={() => ref.current?.focus()}>Focus</button>
+      <button onClick={() => ref.current?.clear()}>Clear</button>
+    </div>
+  )
+}
 
 Basic.args = {}
