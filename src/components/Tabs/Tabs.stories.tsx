adecdcc4a474662e2098dfb9c631d0133ffc657e
Swaraj Cheguri <73115842+swarajcheguri-harness@users.noreply.github.com>
2021-07-26 13:24:22 +0530

    feat: [PL-15210]: redesign tabs for icons (#349)
    * feat: [PL-15210]: redesign tabs for icons
    
    * update index file
    
    * backward compatbiltity
    
    * type issues correction
    
    * update border height

--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -1,24 +1,56 @@
 import React from 'react'
 import type { Meta, Story } from '@storybook/react'
 
-import { Tabs, Tab, TabsProps } from './Tabs'
+import { Tabs, TabsProps } from './Tabs'
 
 export default {
   title: 'Components / Tabs',
   component: Tabs
 } as Meta
-
 export const Horizontal: Story<TabsProps> = () => (
-  <Tabs id={'horizontalTabs'} defaultSelectedTabId={'tab1'}>
-    <Tab id={'tab1'} title={'Tab 1 title'} panel={<div>Tab 1 content</div>} />
-    <Tab id={'tab2'} title={'Tab 2 title'} panel={<div>Tab 2 content</div>} />
-    <Tab id={'tab3'} title={'Tab 3 title'} panel={<div>Tab 3 content</div>} disabled />
-  </Tabs>
+  <Tabs
+    id={'horizontalTabs'}
+    defaultSelectedTabId={'tab1'}
+    tabList={[
+      { id: 'tab1', title: 'Tab 1 title', panel: <div>Tab 1 content</div> },
+      { id: 'tab2', title: 'Tab 2 title', panel: <div>Tab 2 content</div> },
+      { id: 'tab3', title: 'Tab 3 title', panel: <div>Tab 3 content</div> }
+    ]}
+  />
 )
+export const HorizontalWithIcons: Story<TabsProps> = () => (
+  <Tabs
+    id={'horizontalTabs'}
+    defaultSelectedTabId={'tab1'}
+    tabList={[
+      { id: 'tab1', title: 'Tab 1 title', panel: <div>Tab 1 content</div>, iconProps: { name: 'gear' } },
+      { id: 'tab2', title: 'Tab 2 title', panel: <div>Tab 2 content</div>, iconProps: { name: 'gear' } },
+      { id: 'tab3', title: 'Tab 3 title', panel: <div>Tab 3 content</div>, iconProps: { name: 'gear' }, disabled: true }
+    ]}></Tabs>
+)
+
 export const Vertical: Story<TabsProps> = () => (
-  <Tabs id={'verticalTabs'} defaultSelectedTabId={'tab1'} vertical>
-    <Tab id={'tab1'} title={'Tab 1 title'} panel={<div>Tab 1 content</div>} />
-    <Tab id={'tab2'} title={'Tab 2 title'} panel={<div>Tab 2 content</div>} disabled />
-    <Tab id={'tab3'} title={'Longer Tab 3 title'} panel={<div>Tab 3 content</div>} />
-  </Tabs>
+  <Tabs
+    id={'verticalTabs'}
+    vertical
+    defaultSelectedTabId={'tab1'}
+    tabList={[
+      { id: 'tab1', title: 'Tab 1 title', panel: <div>Tab 1 content</div> },
+      { id: 'tab2', title: 'Tab 2 title', panel: <div>Tab 2 content</div> },
+      { id: 'tab3', title: 'Tab 3 title', panel: <div>Tab 3 content</div> }
+    ]}
+  />
+)
+
+export const VerticalWithIcons: Story<TabsProps> = () => (
+  <Tabs
+    id={'verticalTabs'}
+    vertical
+    defaultSelectedTabId={'tab1'}
+    tabList={[
+      { id: 'tab1', title: 'Tab 1 title', panel: <div>Tab 1 content</div>, iconProps: { name: 'gear' } },
+      { id: 'tab2', title: 'Tab 2 title', panel: <div>Tab 2 content</div>, iconProps: { name: 'gear' } },
+      { id: 'tab3', title: 'Tab 3 title', panel: <div>Tab 3 content</div>, iconProps: { name: 'gear' } }
+    ]}
+  />
 )
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,25 +1,54 @@
 import React from 'react'
-import { Tabs as BpTabs, Tab, ITabsProps } from '@blueprintjs/core'
+import { Tabs as BpTabs, Tab, ITabsProps, ITabProps } from '@blueprintjs/core'
 import css from './Tabs.css'
 import cx from 'classnames'
 
+import { Icon, IconProps } from '../../icons/Icon'
+
+interface TabProps extends ITabProps {
+  iconProps?: IconProps
+}
 interface TabsProps extends Omit<ITabsProps, 'renderActiveTabPanelOnly'> {
   renderAllTabPanels?: boolean
   children?: React.ReactNode
+  // TODO: Remove optional field for tablist when old tab code in ng  is migrated to tablist props usage
+  tabList?: TabProps[]
+}
+
+function tab(props: TabProps) {
+  const { title, iconProps, ...rest } = props
+  const titleContent = iconProps ? (
+    <span>
+      <Icon {...iconProps} padding={{ right: 'small' }} />
+      {title}
+    </span>
+  ) : (
+    title
+  )
+  return <Tab title={titleContent} {...rest} />
 }
 
 function Tabs(props: TabsProps) {
-  const { renderAllTabPanels, children, vertical, ...rest } = props
-
+  const { renderAllTabPanels, vertical, children, tabList = [], ...rest } = props
+  const hasIcons = tabList.findIndex(tabProp => tabProp.iconProps?.name) !== -1
   return (
     <BpTabs
       {...rest}
       vertical={vertical}
       renderActiveTabPanelOnly={!renderAllTabPanels}
-      className={cx(css.main, { [css.vertical]: vertical, [css.horizontal]: !vertical })}>
+      className={cx(css.main, {
+        [css.vertical]: vertical,
+        [css.horizontal]: !vertical,
+        [css.icons]: hasIcons && !vertical
+      })}>
+      {tabList.map(tabProp => {
+        return tab(tabProp)
+      })}
+      {/* TODO: Remove children rendering when old tab code in ng  is migrated to tablist props usage */}
+      {/* rendering with children is for backward compatbility. This will be removed once all the code is moved using tablist props */}
       {children}
     </BpTabs>
   )
 }
 
-export { Tabs, Tab, TabsProps }
+export { Tabs, TabProps, Tab, TabsProps }
