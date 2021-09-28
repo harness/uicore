0b24fa4ca2b9378d78a2ff6f2612b92b40629cf6
Abhinav Rastogi <abhinav.rastogi@harness.io>
2021-09-28 18:40:08 +0530

    feat: [PL-20047]: Pagination design update

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -11,11 +11,34 @@ export default {
 export const Basic: Story<PaginationProps> = args => {
   const [pageIndex, setPageIndex] = useState(args.pageIndex || 0)
 
-  return <Pagination {...args} pageIndex={pageIndex} gotoPage={setPageIndex} nextPage={setPageIndex} />
+  return <Pagination {...args} pageIndex={pageIndex} gotoPage={setPageIndex} />
 }
 
 Basic.args = {
   itemCount: 147,
   pageCount: 15,
   pageSize: 10
+}
+
+export const VariablePageSize: Story<PaginationProps> = args => {
+  const [pageIndex, setPageIndex] = useState(args.pageIndex || 0)
+  const [pageSize, setpageSize] = useState(args.pageSize || 0)
+
+  return (
+    <Pagination
+      {...args}
+      pageSize={pageSize}
+      pageIndex={pageIndex}
+      gotoPage={setPageIndex}
+      pageSizeOptions={args.pageSizeOptions}
+      onPageSizeChange={setpageSize}
+    />
+  )
+}
+
+VariablePageSize.args = {
+  itemCount: 147,
+  pageCount: 15,
+  pageSize: 10,
+  pageSizeOptions: [5, 10, 25, 50]
 }
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,23 +1,31 @@
 import React from 'react'
 import { Layout } from '../../layouts/Layout'
 import { Text } from '../Text/Text'
-import { Button } from '../Button/Button'
-import { SelectV2 } from '../Select/SelectV2'
+import { Button, ButtonSize } from '../Button/Button'
+import cx from 'classnames'
+import { DropDown } from '../DropDown/DropDown'
+import { SelectOption } from '../Select/Select'
+import { FontVariation } from '../../styled-props/font/FontProps'
 
 import css from './Pagination.css'
 
 export interface PaginationProps {
   pageSize: number
-  pageIndex?: number
   pageCount: number
   itemCount: number
-  gotoPage: (index: number) => void
-  nextPage?: (index: number) => void
-  pageCountClamp?: number
+  pageIndex?: number
+  gotoPage?: (index: number) => void
+  pageSizeOptions?: number[]
+  onPageSizeChange?: (newPageSize: number) => void
 }
 
 const Pagination: React.FC<PaginationProps> = props => {
-  const { pageSize, pageIndex = 0, nextPage, pageCount, itemCount, gotoPage, pageCountClamp = 5 } = props
+  const { pageSize, pageIndex = 0, gotoPage, pageCount, itemCount, pageSizeOptions, onPageSizeChange } = props
+
+  const selectOptions: SelectOption[] =
+    pageSizeOptions?.map(option => {
+      return { label: option.toString(), value: option.toString() }
+    }) || []
 
   if (pageIndex >= pageCount) {
     // eslint-disable-next-line no-console
@@ -30,28 +38,6 @@ const Pagination: React.FC<PaginationProps> = props => {
       `Pagination: pageSize (${pageSize}) * pageCount (${pageCount}) can't be less than itemCount (${itemCount})`
     )
   }
-  if (pageCountClamp < 5) {
-    // eslint-disable-next-line no-console
-    console.warn(`Pagination: pageCountClamp (${pageCountClamp}) can't be less than 5`)
-    return null
-  }
-
-  const moreLeft = pageIndex + 1 > pageCountClamp
-  const moreRight = pageCount - pageIndex > pageCountClamp
-
-  const renderPage = (pageNumber: number) => {
-    const isCurrent = pageIndex + 1 === pageNumber
-    return (
-      <Button
-        key={`page-${pageNumber}`}
-        minimal={!isCurrent}
-        text={pageNumber}
-        intent={isCurrent ? 'primary' : 'none'}
-        disabled={isCurrent}
-        onClick={() => gotoPage(pageNumber - 1)}
-      />
-    )
-  }
 
   return (
     <Layout.Horizontal
@@ -59,83 +45,53 @@ const Pagination: React.FC<PaginationProps> = props => {
       flex={{ align: 'center-center', distribution: 'space-between' }}
       className={css.container}>
       {itemCount <= pageSize ? (
-        // eg. Showing 3 of 3
-        <Text inline className={css.text}>
-          Showing <strong>{itemCount}</strong> of <strong>{itemCount}</strong>
+        // eg. 3 of 3
+        <Text inline font={{ variation: FontVariation.SMALL_SEMI }}>
+          {itemCount} of {itemCount}
         </Text>
       ) : (
-        // eg. Showing 1 - 10 of 50
-        <Text inline className={css.text}>
-          Showing <strong>{pageSize * pageIndex + 1}</strong> -{' '}
-          <strong>{Math.min(pageSize * (pageIndex + 1), itemCount)}</strong> of <strong>{itemCount}</strong>
+        // eg. (1-10) of 50
+        <Text inline font={{ variation: FontVariation.SMALL_SEMI }}>
+          {`(${pageSize * pageIndex + 1} - ${Math.min(pageSize * (pageIndex + 1), itemCount)}) of ${itemCount}`}
         </Text>
       )}
-      {itemCount > pageSize ? (
-        <Layout.Horizontal spacing="small">
-          {pageCount <= pageCountClamp * 2 ? (
-            Array.from(Array(pageCount).keys()).map(pageNumber => {
-              const isCurrent = pageIndex === pageNumber
-              return (
-                <Button
-                  key={`page-${pageNumber}`}
-                  minimal={!isCurrent}
-                  text={pageNumber + 1}
-                  intent={isCurrent ? 'primary' : 'none'}
-                  disabled={isCurrent}
-                  onClick={() => gotoPage(pageNumber)}
-                />
-              )
-            })
-          ) : (
-            <React.Fragment>
-              {Array.from({ length: moreLeft ? 2 : pageCountClamp }, (_, i) => renderPage(i + 1))}
-              {moreLeft && (
-                <SelectV2
-                  items={Array.from(Array(moreRight ? pageIndex - 3 : pageCount - pageCountClamp - 2).keys()).map(
-                    pageNumber => {
-                      return {
-                        label: `${pageNumber + 3}`,
-                        value: pageNumber + 2
-                      }
-                    }
-                  )}
-                  onChange={item => gotoPage(item.value as number)}
-                  filterable={false}>
-                  <Button icon="more" minimal width="100%" />
-                </SelectV2>
-              )}
-              {moreLeft && moreRight && Array.from({ length: 3 }, (_, i) => renderPage(pageIndex + i))}
-              {moreRight && (
-                <SelectV2
-                  items={Array.from(
-                    Array(moreLeft ? pageCount - pageIndex - 4 : pageCount - pageCountClamp - 2).keys()
-                  ).map(pageNumber => {
-                    return {
-                      label: `${pageNumber + (moreLeft ? pageIndex + 3 : pageCountClamp + 1)}`,
-                      value: pageNumber + (moreLeft ? pageIndex + 2 : pageCountClamp)
-                    }
-                  })}
-                  onChange={item => gotoPage(item.value as number)}
-                  filterable={false}>
-                  <Button icon="more" minimal width="100%" />
-                </SelectV2>
-              )}
-              {moreRight
-                ? [pageCount - 1, pageCount].map(i => renderPage(i))
-                : Array.from({ length: pageCountClamp }, (_, i) => renderPage(i + 1 + pageCount - pageCountClamp))}
-            </React.Fragment>
-          )}
-          {pageCount > pageCountClamp && nextPage ? (
-            <Button
-              minimal
-              icon="chevron-right"
-              onClick={() => nextPage(pageIndex + 1)}
-              disabled={pageIndex + 1 === pageCount}
-              style={{ marginRight: 'var(--spacing-medium)' }}
-            />
-          ) : null}
+      <Layout.Horizontal>
+        <Button
+          text="Prev"
+          icon="arrow-left"
+          size={ButtonSize.SMALL}
+          className={cx(css.roundedButton, css.buttonLeft)}
+          iconProps={{ size: 12 }}
+          onClick={() => gotoPage?.(pageIndex - 1)}
+          disabled={pageIndex === 0}
+        />
+        <Button
+          text="Next"
+          rightIcon="arrow-right"
+          size={ButtonSize.SMALL}
+          className={cx(css.roundedButton, css.buttonRight)}
+          iconProps={{ size: 12 }}
+          onClick={() => gotoPage?.(pageIndex + 1)}
+          disabled={pageIndex === pageCount - 1}
+        />
+      </Layout.Horizontal>
+
+      {pageSizeOptions && onPageSizeChange ? (
+        <Layout.Horizontal flex={{ alignItems: 'baseline' }} spacing="small">
+          <Text>Show</Text>
+          <DropDown
+            items={selectOptions}
+            onChange={item => onPageSizeChange(item.value as number)}
+            value={pageSize.toString()}
+            filterable={false}
+            width={60}
+            className={css.pageSizeDropdown}
+          />
+          <Text>per page</Text>
         </Layout.Horizontal>
-      ) : null}
+      ) : (
+        <span>{`Show ${pageSize} per page`}</span>
+      )}
     </Layout.Horizontal>
   )
 }
