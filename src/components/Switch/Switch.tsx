37a9099f7ec71981dc775f63c470e718431cf0c2
Abhinav Rastogi <abhinav.rastogi@harness.io>
2021-10-11 11:16:10 +0530

    feat: add tooltip support in Switch component (#417)
    * feat: add tooltip support in Switch component
    
    * feat: add tooltip support in Switch component
    
    * feat: Passed the tooltip ID in switch and checked the usage in NG
    
    * feat: use labelText
    
    Co-authored-by: Chirag Parihar <chirag.parihar@harness.io>

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,10 +1,13 @@
-import { Assign } from 'utility-types'
-import { StyledProps, omitStyledProps, styledClasses } from '../../styled-props/StyledProps'
-import { Switch as BpSwitch, ISwitchProps } from '@blueprintjs/core'
 import React, { FormEvent } from 'react'
+import { Assign } from 'utility-types'
+import { Switch as BpSwitch, ISwitchProps } from '@blueprintjs/core'
+import { omit } from 'lodash-es'
+import { StyledProps, omitStyledProps, styledClasses } from '../../styled-props/StyledProps'
 import styledClass from '../../styled-props/StyledProps.css'
 
 import css from './Switch.css'
+import { HarnessDocTooltip } from '../../frameworks/Tooltip/Tooltip'
+import type { TooltipRenderProps } from '../../frameworks/Tooltip/types'
 
 export interface SwitchProps extends Assign<Omit<ISwitchProps, 'onChange'>, StyledProps> {
   /** onChange event handler */
@@ -12,15 +15,18 @@ export interface SwitchProps extends Assign<Omit<ISwitchProps, 'onChange'>, Styl
 
   /** className to be appended to default className */
   className?: string
+
+  /** props for adding documentation tooltip */
+  tooltipProps?: TooltipRenderProps
 }
 
-export function Switch(props: SwitchProps) {
-  const { className = '', onChange } = props
+export function Switch(props: SwitchProps): React.ReactElement {
+  const { className = '', tooltipProps, label } = props
 
   return (
     <BpSwitch
-      {...omitStyledProps(props)}
-      onChange={onChange}
+      labelElement={<HarnessDocTooltip labelText={label} {...tooltipProps} className={css.inlineTooltip} />}
+      {...omitStyledProps(omit(props, ['tooltipProps', 'label']))}
       className={styledClasses(props, styledClass.font, css.switch, className)}
     />
   )
