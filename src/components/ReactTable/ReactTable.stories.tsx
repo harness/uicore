0606fe07af413e380b6c27cf57e06f114b5e40ce
sahithikolichala <57372986+sahithikolichala@users.noreply.github.com>
2021-11-01 22:04:39 -0700

    feat: [GIT-217]: moved the table component from ng to uicore (#470)
    * feat: [GIT-217]: moved the table component from ng to uicore
    
    * fix: fixed prettier
    
    * fix: fixed the eslint issue
    
    * fix: updated snapshots
    
    * fix: reverted back yarn.lock
    
    * fix: fixed prettier

---
+++ src/components/ReactTable/ReactTable.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import type { Story, Meta } from '@storybook/react'
+
+import ReactTable, { TableProps } from './ReactTable'
+
+interface Data {
+  name: string
+  age: number
+}
+
+export default {
+  title: 'Common / Table',
+  component: ReactTable,
+  argTypes: {}
+} as Meta
+
+const Template: Story<TableProps<Data>> = args => <ReactTable {...args} sortable={true} />
+
+export const Basic = Template.bind({})
+
+Basic.args = {
+  columns: [
+    {
+      Header: 'Name',
+      accessor: row => row.name,
+      id: 'name'
+    },
+    {
+      Header: 'Age',
+      accessor: row => row.age,
+      id: 'age'
+    }
+  ],
+  data: [
+    {
+      name: 'User 1',
+      age: 20
+    },
+    {
+      name: 'User 2',
+      age: 25
+    }
+  ]
+}
