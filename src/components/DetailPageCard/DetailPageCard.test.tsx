643e62f945e90dbb27dc23221e6d002cf5b31052
Calvin <calvin.lee@harness.io>
2021-11-30 16:38:56 -0700

    feat: fixed spacing

--- src/components/DetailPageCard/DetailPageCard.test.tsx
+++ src/components/DetailPageCard/DetailPageCard.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import DetailPagecard from './DetailPagecard'
-
 describe('Detail Page card', () => {
   test('should render DetailPagecard with title and content', () => {
     const { container } = render(
