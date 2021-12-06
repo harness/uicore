c7bab25880ad5cae2e5a937d10d2dc7ad3e55836
Vivek Kumar Bansal <vivek.bansal@harness.io>
2021-12-06 11:12:10 +0530

    feat: add support for custom class names to dialog

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import cx from 'classnames'
 import { Container, Icon } from '../..'
 import { Dialog as BluePrintDialog, IDialogProps } from '@blueprintjs/core'
 import css from './Dialog.css'
@@ -6,6 +7,7 @@ import css from './Dialog.css'
 export interface DialogProps extends IDialogProps {
   footer?: JSX.Element | JSX.Element[]
   children?: JSX.Element | JSX.Element[]
+  chidrenClassName?: string
 }
 
 const defaultProps = {
@@ -16,11 +18,15 @@ const defaultProps = {
   canEscapeKeyClose: true
 }
 
-export function Dialog(props: DialogProps) {
+export function Dialog(props: DialogProps): React.ReactElement {
   return (
-    <BluePrintDialog {...defaultProps} {...props} className={css.dialog} isCloseButtonShown={false}>
+    <BluePrintDialog
+      {...defaultProps}
+      {...props}
+      className={cx(css.dialog, props.className)}
+      isCloseButtonShown={false}>
       <Icon name="Stroke" size={15} onClick={props.onClose} className={css.close} />
-      <Container padding="none" className={css.children}>
+      <Container padding="none" className={cx(css.children, props.chidrenClassName)}>
         {props.children}
       </Container>
       <footer>{props.footer}</footer>
