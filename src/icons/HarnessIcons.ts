736ff2b731a781bc06590b21b1e00378afce75bf
avinashmadhwani02 <62028553+avinashmadhwani02@users.noreply.github.com>
2021-12-15 14:37:18 +0530

    fix: Removed settings icon

--- src/icons/HarnessIcons.ts
+++ src/icons/HarnessIcons.ts
@@ -540,7 +540,6 @@ import ServiceSumologic from './service-sumologic.svg'
 import ServiceTerraform from './service-terraform.svg'
 import Service from './service.svg'
 import Services from './services.svg'
-import Settings from './settings.svg'
 import SetupApi from './setup-api.svg'
 import SetupTags from './setup-tags.svg'
 import Skipped from './skipped.svg'
@@ -1150,7 +1149,6 @@ type HarnessIconName =
   | 'service-terraform'
   | 'service'
   | 'services'
-  | 'settings'
   | 'setup-api'
   | 'setup-tags'
   | 'skipped'
@@ -1760,7 +1758,6 @@ const HarnessIcons: KVO<FunctionComponent<ElementType>> = {
   'service-terraform': ServiceTerraform,
   service: Service,
   services: Services,
-  settings: Settings,
   'setup-api': SetupApi,
   'setup-tags': SetupTags,
   skipped: Skipped,
