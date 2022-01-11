26cd92ae4a39a8974501ee096334664fecfa45c9
Mayank Verma <mayank.verma@harness.io>
2022-01-11 23:22:15 +0530

    fix: [PL-20844]: FormError now accepts a className

--- src/components/FormError/FormError.tsx
+++ src/components/FormError/FormError.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { FormikErrors } from 'formik'
+import cx from 'classnames'
 import { Icon } from '../../icons/Icon'
 import css from './FormError.css'
 
-export const FormError = ({ errorMessage }: { errorMessage: string | undefined | FormikErrors<any> }) => {
+export const FormError = ({
+  errorMessage,
+  className
+}: {
+  errorMessage: string | undefined | FormikErrors<any>
+  className?: string
+}) => {
   if (!errorMessage) {
     return null
   }
   // Used to display form errors below the fields, with an icon
   return (
-    <div className={css.errorDiv}>
+    <div className={cx(css.errorDiv, className)}>
       <Icon name="circle-cross" className={css.errorTextIcon} size={12} intent="danger" />
       <span className={css.error}>{errorMessage}</span>
     </div>
