3cbd851a34fba4e3aaf3d2729e67dd54ed864bc0
Mayank Verma <mayank.verma@harness.io>
2022-01-13 14:19:03 +0530

    fix: [PL-21212]: review comments - used design system components and minimized CSS usage

--- src/components/FormError/FormError.tsx
+++ src/components/FormError/FormError.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { FormikErrors } from 'formik'
-import { Icon } from '../../icons/Icon'
+import { Layout, Text, Color, Icon } from '../../'
 import css from './FormError.css'
 
 export const FormError = ({ errorMessage }: { errorMessage: string | undefined | FormikErrors<any> }) => {
   if (!errorMessage) {
     return null
   }
+
   // Used to display form errors below the fields, with an icon
   return (
-    <div className={css.errorDiv}>
-      <Icon name="circle-cross" className={css.errorTextIcon} size={12} intent="danger" />
-      <span className={css.error}>{errorMessage}</span>
-    </div>
+    <Layout.Horizontal margin={{ top: 'small' }}>
+      <Icon className={css.errorTextIcon} name="circle-cross" margin={{ right: 'xsmall' }} size={12} intent="danger" />
+      <Text className={css.error} color={Color.RED_600}>
+        {errorMessage}
+      </Text>
+    </Layout.Horizontal>
   )
 }
