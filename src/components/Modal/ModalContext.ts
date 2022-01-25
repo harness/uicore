4ae26efcf5320a4c46b5bedbbbcd9c07ee838c3b
Vivek Kumar Bansal <vivek.bansal@harness.io>
2022-01-25 15:28:24 +0530

    feat: remove error throw from useModal

--- src/components/Modal/ModalContext.ts
+++ src/components/Modal/ModalContext.ts
@@ -23,18 +23,9 @@ export interface ModalContextType {
 }
 
 /**
- * Throw error when ModalContext is used outside of context provider
- */
-const invariantViolation = () => {
-  throw new Error(
-    'Attempted to call useModal outside of modal context. Make sure your app is rendered inside ModalProvider.'
-  )
-}
-
-/**
  * Modal Context Object
  */
 export const ModalContext = React.createContext<ModalContextType>({
-  showModal: invariantViolation,
-  hideModal: invariantViolation
+  showModal: () => void 0,
+  hideModal: () => void 0
 })
