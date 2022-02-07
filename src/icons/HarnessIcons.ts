e5dab80578d0f8fa409ab34993a1d78c1d6d329d
Kapil Kumawat <erkapil2006@gmail.com>
2022-02-07 10:26:19 +0530

    Update HarnessIcons.ts

--- src/icons/HarnessIcons.ts
+++ src/icons/HarnessIcons.ts
@@ -4,6 +4,7 @@
  * that can be found in the licenses directory at the root of this repository, also available at
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
+
 /**
  * This file is auto-generated. Please do not modify this file manually.
  * Use the command `yarn ui:icons` to regenerate this file.
