3b29d3c227b78f1377b78e8ec6a1b867852fa8b9
avinashmadhwani02 <62028553+avinashmadhwani02@users.noreply.github.com>
2022-02-23 14:39:04 +0530

    fix: Removed exports from index.ts in uicore

--- src/index.ts
+++ src/index.ts
@@ -215,4 +215,3 @@ export {
 export { DetailPageCard, DetailPageCardProps, Content, ContentType } from './components/DetailPageCard/DetailPageCard'
 export { SimpleLogViewer, SimpleLogViewerProps } from './components/LogViewer/SimpleLogViewer'
 export { breakOnLinks, LogLineProps, LogLine, MemoizedLogLine } from './components/LogViewer/LogLine'
-export * from '@harness/design-system'
