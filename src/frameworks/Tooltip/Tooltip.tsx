662cb370fc08cec41b3532f0721ac9607d27d43e
Vivek Kumar Bansal <5930351+vkbansal@users.noreply.github.com>
2022-03-24 14:00:20 +0530

    chore: upgrade to marked v4 (#626)
    * chore: upgrade to marked v4
    
    * chore: upgrade to marked v4
    
    * chore: upgrade to marked v4

--- src/frameworks/Tooltip/Tooltip.tsx
+++ src/frameworks/Tooltip/Tooltip.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react'
 import cx from 'classnames'
-import marked from 'marked'
+import { marked } from 'marked'
 import { PopoverInteractionKind } from '@blueprintjs/core'
 import { useTooltips } from './TooltipContext'
 import { TooltipRenderProps } from './types'
