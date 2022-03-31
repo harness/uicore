be4df9456102076f02b5bde8379d0899ebe55105
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2022-03-31 22:04:45 +0530

    feat: Added Azure Blob icon

--- src/icons/HarnessIcons.ts
+++ src/icons/HarnessIcons.ts
@@ -37,6 +37,7 @@ import AwsCodecommit from './aws-codecommit.svg'
 import AwsKms from './aws-kms.svg'
 import AwsRds from './aws-rds.svg'
 import AwsSecretManager from './aws-secret-manager.svg'
+import AzureBlob from './azure-blob.svg'
 import AzureKeyVault from './azure-key-vault.svg'
 import AzureKubernetesService from './azure-kubernetes-service.svg'
 import Banned from './banned.svg'
@@ -734,6 +735,7 @@ type HarnessIconName =
   | 'aws-kms'
   | 'aws-rds'
   | 'aws-secret-manager'
+  | 'azure-blob'
   | 'azure-key-vault'
   | 'azure-kubernetes-service'
   | 'banned'
@@ -1431,6 +1433,7 @@ const HarnessIcons: KVO<ElementType> = {
   'aws-kms': AwsKms,
   'aws-rds': AwsRds,
   'aws-secret-manager': AwsSecretManager,
+  'azure-blob': AzureBlob,
   'azure-key-vault': AzureKeyVault,
   'azure-kubernetes-service': AzureKubernetesService,
   banned: Banned,
