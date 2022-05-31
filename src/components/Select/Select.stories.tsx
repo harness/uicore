5452a78145f2e32506af0d3372cc40b5d69db196
Abhinav Rastogi <abhinav.rastogi@harness.io>
2022-05-31 14:21:50 -0700

    fix: [PL-24968]: should clear false values

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -10,7 +10,7 @@ import type { Meta, Story } from '@storybook/react'
 import { Title, Subtitle, ArgsTable, Stories, PRIMARY_STORY, Primary, Description } from '@storybook/addon-docs/blocks'
 import { Select, SelectProps } from '../..'
 import { omit } from 'lodash-es'
-import { SelectSize } from './Select'
+import { SelectOption, SelectSize } from './Select'
 import data from '../../_stories/components/pokedex.json'
 
 export default {
@@ -44,20 +44,22 @@ export default {
   },
   decorators: [Story => <Story />]
 } as Meta
+
 export const Basic: Story<SelectProps> = args => {
-  const {
-    items = [
-      { label: 'Kubernetes', value: 'service-kubernetes' },
-      { label: 'GitHub', value: 'service-github' },
-      { label: 'ELK', value: 'service-elk' },
-      { label: 'Jenkins', value: 'service-jenkins' },
-      { label: 'GCP', value: 'service-gcp' }
-    ]
-  } = args
-  const argsCopy = omit(args, ['size', 'items'])
-  return <Select items={items} addClearBtn={true} {...argsCopy} />
+  const [value, setValue] = useState<SelectOption | null | undefined>(args.value || null)
+  return <Select {...args} value={value} onChange={item => setValue(item)} />
 }
-Basic.args = { size: SelectSize.Large, borderless: false }
+Basic.args = {
+  size: SelectSize.Medium,
+  items: [
+    { label: 'Kubernetes', value: 'service-kubernetes' },
+    { label: 'GitHub', value: 'service-github' },
+    { label: 'ELK', value: 'service-elk' },
+    { label: 'Jenkins', value: 'service-jenkins' },
+    { label: 'GCP', value: 'service-gcp' }
+  ]
+}
+
 export const SelectWithIcons: Story<SelectProps> = args => {
   const {
     items = [
