c9892bb985f68e34e0ae78c36a98c1686a8a5793
Uday Vunnam <20707504+udayvunnam@users.noreply.github.com>
2022-07-07 15:24:35 +0530

    feat: [PIE-4356]: expandable table rows (#747)

--- packages/uicore/src/components/TableV2/TableV2.stories.tsx
+++ packages/uicore/src/components/TableV2/TableV2.stories.tsx
@@ -5,16 +5,11 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-import React from 'react'
-import type { Story, Meta } from '@storybook/react'
+import type { ComponentStory, Meta } from '@storybook/react'
 import { noop } from 'lodash-es'
-
-import { TableV2, TableProps } from './TableV2'
-
-interface Data {
-  name: string
-  age: number
-}
+import React from 'react'
+import { Icon } from '../..'
+import { TableV2 } from './TableV2'
 
 export default {
   title: 'Components / TableV2',
@@ -22,12 +17,72 @@ export default {
   argTypes: {}
 } as Meta
 
-const Template: Story<TableProps<Data>> = args => <TableV2 {...args} sortable={true} />
-
-export const Basic = Template.bind({})
-
+export const Basic: ComponentStory<typeof TableV2> = args => {
+  return <TableV2 {...args} sortable={true} onRowClick={noop} />
+}
 Basic.args = {
+  data: [
+    {
+      name: 'User 1',
+      age: 20
+    },
+    {
+      name: 'User 2',
+      age: 25
+    },
+    {
+      name: 'User 3',
+      age: 25
+    },
+    {
+      name: 'User 4',
+      age: 25
+    }
+  ],
+  pagination: {
+    itemCount: 100,
+    pageCount: 10,
+    pageSize: 10,
+    gotoPage: noop,
+    pageIndex: 0
+  },
   columns: [
+    {
+      Header: 'Name',
+      accessor: row => row.name,
+      id: 'name'
+    },
+    {
+      Header: 'Age',
+      accessor: row => row.age,
+      id: 'age'
+    }
+  ]
+}
+
+export const Expandable: ComponentStory<typeof TableV2> = args => {
+  const renderRowSubComponent = React.useCallback(
+    ({ row }) => (
+      <pre style={{ margin: 0 }}>
+        <code>{JSON.stringify({ values: row.values }, null, 2)}</code>
+      </pre>
+    ),
+    []
+  )
+
+  return <TableV2 {...args} sortable={true} renderRowSubComponent={renderRowSubComponent} />
+}
+Expandable.args = {
+  columns: [
+    {
+      Header: '',
+      width: '30px',
+      id: 'expander',
+      // eslint-disable-next-line react/display-name
+      Cell: ({ row }) => {
+        return <Icon name={row.isExpanded ? 'chevron-down' : 'chevron-right'} {...row.getToggleRowExpandedProps()} />
+      }
+    },
     {
       Header: 'Name',
       accessor: row => row.name,
