bad366ff439d550338c730df7a322b61317eeac6
Vivek Kumar Bansal <5930351+vkbansal@users.noreply.github.com>
2022-07-07 18:35:33 +0530

    feat: [PIE-4316]: update multitype menu interaction  @vkbansal (#751)
    * feat: [PIE-4316]: update multitype menu interaction
    
    * feat: [PIE-4316]: update multitype menu interaction
    
    * feat: [PIE-4316]: update multitype menu interaction
    
    Co-authored-by: Ishant Mehta <43949150+ishantmehtaharness@users.noreply.github.com>

---
+++ packages/uicore/src/components/MultiTypeInput/LearnMorePopover.tsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import { I18nResource } from '@harness/design-system'
+import { Popover, PopoverPosition } from '@blueprintjs/core'
+import { Icon } from '@harness/icons'
+import cx from 'classnames'
+
+import { MultiTypeInputType, MultiTypeIcon as TypeIcon, MultiTypeIconSize as TypeIconSize } from './MultiTypeInputUtils'
+
+import { Button, ButtonVariation } from '../Button/Button'
+import { Checkbox } from '../Checkbox/Checkbox'
+
+import css from './MultiTypeInput.css'
+
+const helperText: Record<MultiTypeInputType, React.ReactNode> = {
+  [MultiTypeInputType.EXPRESSION]: (
+    <React.Fragment>
+      <b>Expressions</b> allow you to use Harness input, output, and execution variables in a setting.
+    </React.Fragment>
+  ),
+  [MultiTypeInputType.FIXED]: (
+    <React.Fragment>
+      <b>Fixed Values</b> are simply values that you enter manually when you configure a setting and do not change at
+      runtime.
+    </React.Fragment>
+  ),
+  [MultiTypeInputType.RUNTIME]: (
+    <React.Fragment>
+      <b>Runtime Inputs</b> are placeholders for values that will be provided when you start a Pipeline execution.
+    </React.Fragment>
+  )
+}
+
+export const labels: Record<MultiTypeInputType, string> = {
+  [MultiTypeInputType.EXPRESSION]: 'expression',
+  [MultiTypeInputType.FIXED]: 'fixedValue',
+  [MultiTypeInputType.RUNTIME]: 'runtimeInput'
+}
+
+export interface LearnMorePopoverProps {
+  i18n: I18nResource
+  type: MultiTypeInputType
+  isLearnMoreOpen: boolean
+  dontShowAgain: boolean
+  setDontShowAgain(val: boolean): void
+  setIsLearnMoreOpen(val: boolean): void
+}
+
+export function LearnMorePopover(props: LearnMorePopoverProps): React.ReactElement {
+  const { i18n, type, isLearnMoreOpen, dontShowAgain, setDontShowAgain, setIsLearnMoreOpen } = props
+
+  function handleChange(e: React.FormEvent<HTMLInputElement>): void {
+    if ((e.target as HTMLInputElement).checked) {
+      setDontShowAgain(true)
+      setIsLearnMoreOpen(false)
+    }
+  }
+
+  const popoverContent = (
+    <div onClick={e => e.stopPropagation()}>
+      <div className={css.learnMore}>
+        <span>{i18n.learnMore}</span>
+        <Button icon="cross" onClick={() => setIsLearnMoreOpen(false)} variation={ButtonVariation.ICON} />
+      </div>
+      <div className={css.body}>
+        <div className={css.content}>
+          <Icon name={TypeIcon[type]} data-type={type} size={TypeIconSize[type] * 1.8} />
+          <span>{helperText[type]}</span>
+        </div>
+
+        {dontShowAgain ? null : (
+          <div className={css.checkbox}>
+            <Checkbox checked={dontShowAgain} onChange={handleChange} label="Don't show again" />
+          </div>
+        )}
+      </div>
+    </div>
+  )
+
+  return (
+    <Popover
+      isOpen={isLearnMoreOpen}
+      content={popoverContent}
+      minimal
+      position={PopoverPosition.LEFT}
+      popoverClassName={css.learnMorePopover}
+      className={css.learnMoreWrapper}
+      modifiers={{ offset: { offset: '0px 0px' }, arrow: { enabled: false } }}>
+      <div className={css.menuItem}>
+        <span className={css.menuItemLabel}>{i18n[labels[type]]}</span>
+        <Icon className={cx(css.menuItemIcon, css[type])} name={TypeIcon[type]} size={TypeIconSize[type]} />
+      </div>
+    </Popover>
+  )
+}
