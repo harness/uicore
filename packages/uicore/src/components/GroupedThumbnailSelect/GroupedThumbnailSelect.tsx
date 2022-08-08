b5132439496e3958ab2a5ba236031a82685aea89
Ashwin Bhatkal <ashwin.bhatkal@harness.io>
2022-08-08 10:26:22 +0530

    fix: remove console log

--- packages/uicore/src/components/GroupedThumbnailSelect/GroupedThumbnailSelect.tsx
+++ packages/uicore/src/components/GroupedThumbnailSelect/GroupedThumbnailSelect.tsx
@@ -51,7 +51,6 @@ const GroupedThumbnailSelect: React.FC<ConnectedGroupedThumbnailSelectProps> = p
 
   const [showAllOptions, setShowAllOptions] = React.useState(isEmpty(value))
   const [visibleGroups, setVisibleGroups] = React.useState(groups)
-  console.log('Groups: ', groups, visibleGroups)
 
   const hasError = errorCheck(name, formik)
   const intent = hasError ? Intent.DANGER : Intent.NONE
