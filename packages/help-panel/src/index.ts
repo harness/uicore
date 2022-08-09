def7b7d56685bb0f5e19d225362726d45bd3f0ee
avinashmadhwani02 <62028553+avinashmadhwani02@users.noreply.github.com>
2022-08-09 13:31:36 +0530

    feat: [PL-27338]: Hide checkbox for content only and export button

--- packages/help-panel/src/index.ts
+++ packages/help-panel/src/index.ts
@@ -8,4 +8,13 @@
 import { HelpPanelContextProvider, useContentful } from './HelpPanelContext'
 import HelpPanel, { HelpPanelType } from './components/HelpPanel/HelpPanel'
 import { HelpPanelEnvironment } from './types/contentfulTypes'
-export { HelpPanel, HelpPanelType, HelpPanelContextProvider, useContentful, HelpPanelEnvironment }
+import FloatingButton, { FloatingButtonProps } from './components/HelpPanel/FloatingButton/FloatingButton'
+export {
+  HelpPanel,
+  HelpPanelType,
+  HelpPanelContextProvider,
+  useContentful,
+  HelpPanelEnvironment,
+  FloatingButton,
+  FloatingButtonProps
+}
