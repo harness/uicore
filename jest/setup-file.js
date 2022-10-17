02aabcfe6c1583078e1a3a207f2e33f39385870a
Ramesh Reddy <ramesh.reddy@harness.io>
2022-10-17 23:11:03 +0530

    chore: mock ResizeObserver

--- jest/setup-file.js
+++ jest/setup-file.js
@@ -18,26 +18,8 @@ global.document.createRange = () => ({
 
 global.window.scrollTo = jest.fn()
 
-class MockIntersectionObserver {
-  constructor() {
-    this.root = null
-    this.rootMargin = ''
-    this.thresholds = []
-    this.disconnect = () => null
-    this.observe = () => null
-    this.takeRecords = () => []
-    this.unobserve = () => null
-  }
-}
-
-Object.defineProperty(window, 'IntersectionObserver', {
-  writable: true,
-  configurable: true,
-  value: MockIntersectionObserver
-})
-
-Object.defineProperty(global, 'IntersectionObserver', {
-  writable: true,
-  configurable: true,
-  value: MockIntersectionObserver
-})
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn()
+}))
