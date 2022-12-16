3e76a2fcfb4e4f8ef8a2c34ff25f392b41b54a64
mayankVermaHarness <mayank.verma@harness.io>
2022-12-16 14:40:18 +0530

    feat: [PL-30311]: small refactor

--- packages/uicore/src/components/Avatar/Avatar.tsx
+++ packages/uicore/src/components/Avatar/Avatar.tsx
@@ -145,7 +145,7 @@ export const Avatar: React.FC<AvatarProps> = (props: AvatarProps) => {
   } else {
     if (!initials) {
       // show empty user Icon if neither 'src', nor 'initials' are truthy
-      const iconSize = parseInt(sizes?.[size].fontSize, 10) as IconProps['size']
+      const iconSize = parseInt(sizes[size].fontSize, 10) as IconProps['size']
       inner = <Icon name="user" color={Color.WHITE} size={iconSize} />
       forcePreventHover = true
     } else {
