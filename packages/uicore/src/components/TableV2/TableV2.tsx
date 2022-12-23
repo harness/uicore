12a249d1974dc052f8b52f28c4ebfe133832f3c4
Swaraj Cheguri <73115842+swarajcheguri-harness@users.noreply.github.com>
2022-12-23 18:26:25 +0530

    feat: [DEL-5541]: auto reset of expanded state option in table v2 (#934)
    * feat: [DEL-5541]: auto reset of expanded state option in table v2
    
    * Update package.json
    
    * review comments

--- packages/uicore/src/components/TableV2/TableV2.tsx
+++ packages/uicore/src/components/TableV2/TableV2.tsx
@@ -44,6 +44,14 @@ export interface TableProps<Data extends Record<string, any>> {
    */
   name?: string
   renderRowSubComponent?: (data: { row: Row<Data> }) => ReactNode
+  /**
+   * When true, the expanded state will automatically reset if any of the following conditions are met:
+      data is changed
+   * To disable, set to false
+      https://react-table-v7.tanstack.com/docs/api/useExpanded#table-options
+   * @default true
+   */
+  autoResetExpanded?: boolean
   autoResetSortBy?: boolean
   autoResetPage?: boolean
 }
@@ -56,6 +64,7 @@ export const TableV2 = <Data extends Record<string, any>>(props: TableProps<Data
     sortable = false,
     resizable = false,
     hideHeaders = false,
+    autoResetExpanded = true,
     pagination,
     rowDataTestID,
     getRowClassName,
@@ -64,7 +73,6 @@ export const TableV2 = <Data extends Record<string, any>>(props: TableProps<Data
     autoResetSortBy = true,
     autoResetPage = true
   } = props
-
   const { headerGroups, page, prepareRow } = useTable(
     {
       columns,
@@ -72,6 +80,7 @@ export const TableV2 = <Data extends Record<string, any>>(props: TableProps<Data
       initialState: { pageIndex: defaultTo(pagination?.pageIndex, 0) },
       manualPagination: true,
       pageCount: defaultTo(pagination?.pageCount, -1),
+      autoResetExpanded,
       autoResetSortBy: autoResetSortBy,
       autoResetPage: autoResetPage
     },
