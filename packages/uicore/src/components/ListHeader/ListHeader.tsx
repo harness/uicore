493672ff1a177f38ea292eebcf55965518a774dc
Avinash Madhwani <avinash.madhwani@harness.io>
2023-04-06 19:52:21 +0530

    fix: created separate component for dropdown

--- packages/uicore/src/components/ListHeader/ListHeader.tsx
+++ packages/uicore/src/components/ListHeader/ListHeader.tsx
@@ -9,42 +9,13 @@ import React from 'react'
 import cx from 'classnames'
 import { FontVariation } from '@harness/design-system'
 import css from './ListHeader.css'
-import { DropDown, Layout, Text, SelectOption } from '../../'
+import { Layout, Text } from '../../'
+import { SortDropdown, SortDropdownProps } from '../SortDropdown/SortDropdown'
 
-interface ListHeaderProps {
+interface ListHeaderProps extends SortDropdownProps {
   totalCount?: number
-  sortOptions: SelectOption[]
-  onSortMethodChange(option: SelectOption): void
-  selectedSortMethod: string
   className?: string
 }
-
-export enum SortMethod {
-  NameAsc = 'name,ASC',
-  NameDesc = 'name,DESC',
-  EmailAsc = 'email,ASC',
-  EmailDesc = 'email,DESC',
-  LastModifiedDesc = 'lastModifiedAt,DESC',
-  Newest = 'createdAt,DESC',
-  Oldest = 'createdAt,ASC'
-}
-
-export const sortByName: SelectOption[] = [
-  { label: 'Name (A->Z, 0->9)', value: SortMethod.NameAsc },
-  { label: 'Name (Z->A, 9->0)', value: SortMethod.NameDesc }
-]
-
-export const sortByEmail: SelectOption[] = [
-  { label: 'Email (A->Z, 0->9)', value: SortMethod.EmailAsc },
-  { label: 'Email (Z->A, 9->0)', value: SortMethod.EmailDesc }
-]
-
-export const sortByLastModified: SelectOption[] = [{ label: 'Last Modified', value: SortMethod.LastModifiedDesc }]
-
-export const sortByCreated: SelectOption[] = [
-  { label: 'Newest', value: SortMethod.Newest },
-  { label: 'Oldest', value: SortMethod.Oldest }
-]
 
 export function ListHeader(props: ListHeaderProps): JSX.Element {
   return (
@@ -53,12 +24,10 @@ export function ListHeader(props: ListHeaderProps): JSX.Element {
       margin={{ bottom: 'small' }}
       className={cx(css.listHeader, props.className)}>
       <Text font={{ variation: FontVariation.H5 }}>Total: {props.totalCount}</Text>
-      <DropDown
-        icon={'main-sort'}
-        items={props.sortOptions}
-        onChange={props.onSortMethodChange}
-        value={props.selectedSortMethod}
-        filterable={false}
+      <SortDropdown
+        sortOptions={props.sortOptions}
+        onSortMethodChange={props.onSortMethodChange}
+        selectedSortMethod={props.selectedSortMethod}
       />
     </Layout.Horizontal>
   )
