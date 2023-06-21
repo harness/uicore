73feedcf7920472c1624ceeb4ae3fbcc44f6de2d
Hrishav <hrishav.kumar@harness.io>
2023-06-21 17:53:17 +0530

    chore: [CHAOS-1895]: Removed Harness Logos and Third Party Logos into a separate package called logos
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- packages/uicore/src/index.ts
+++ packages/uicore/src/index.ts
@@ -264,3 +264,4 @@ export {
 export { StringSubstitute, stringSubstitute } from './components/StringSubstitute/StringSubstitute'
 export * from '@harness/design-system'
 export * from '@harness/icons'
+export * from '../../logos'
