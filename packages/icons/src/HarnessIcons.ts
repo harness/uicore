04c3535cd90d3035897b789a3a8f88e44e394654
Hrishav <hrishav.kumar@harness.io>
2023-06-26 21:28:09 +0530

    fix: [CHAOS-1895]: Added new litmus icons
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- packages/icons/src/HarnessIcons.ts
+++ packages/icons/src/HarnessIcons.ts
@@ -102,6 +102,8 @@ import CfHover from './cf-hover.svg'
 import ChainedPipelineHover from './chained-pipeline-hover.svg'
 import ChainedPipeline from './chained-pipeline.svg'
 import ChangeLog from './change-log.svg'
+import ChaosChaoshubPrivate from './chaos-chaoshub-private.svg'
+import ChaosChaoshubPublic from './chaos-chaoshub-public.svg'
 import ChaosCube from './chaos-cube.svg'
 import ChaosExperimentWeight from './chaos-experiment-weight.svg'
 import ChaosHubs from './chaos-hubs.svg'
@@ -875,6 +877,8 @@ type HarnessIconName =
   | 'chained-pipeline-hover'
   | 'chained-pipeline'
   | 'change-log'
+  | 'chaos-chaoshub-private'
+  | 'chaos-chaoshub-public'
   | 'chaos-cube'
   | 'chaos-experiment-weight'
   | 'chaos-hubs'
@@ -1648,6 +1652,8 @@ const HarnessIcons: KVO<ElementType> = {
   'chained-pipeline-hover': ChainedPipelineHover,
   'chained-pipeline': ChainedPipeline,
   'change-log': ChangeLog,
+  'chaos-chaoshub-private': ChaosChaoshubPrivate,
+  'chaos-chaoshub-public': ChaosChaoshubPublic,
   'chaos-cube': ChaosCube,
   'chaos-experiment-weight': ChaosExperimentWeight,
   'chaos-hubs': ChaosHubs,
