df4ea4580e7a5a1300da6101c3d87c8321c55839
Pratyush Garg <100121280+PratyushGar@users.noreply.github.com>
2023-08-01 12:32:36 +0530

    fix: [CDS-75452]: suppress onInteraction warning + optimise ExpressionDropdown ( Resize Observer limit reacher Error ) (#1122)
    * fix: [CDS-75452]: supress onInteraction warning + optimise ExpressionDropdown
    
    * fix: fixes
    
    * fix: fixes

--- packages/uicore/src/components/ExpressionDropdown/ExpressionDropdown.tsx
+++ packages/uicore/src/components/ExpressionDropdown/ExpressionDropdown.tsx
@@ -33,117 +33,13 @@ interface NewExpressionDropdownProps {
   itemRenderer: React.ReactNode
 }
 
-interface getVisibleItemRendererProps {
-  dropDownItemClickHandler: (value: string) => void
-  isOpen: boolean[]
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean[]>>
-}
-
-function getOverflowRenderer(props: getVisibleItemRendererProps): any {
-  const { dropDownItemClickHandler, isOpen, setIsOpen } = props
-
-  const [isPopoverOpen, setIsPopoverOpen] = React.useState<boolean>(false)
-
-  // eslint-disable-next-line react/display-name
-  return (items: TrieNode[]): JSX.Element => {
-    function itemClickHandler(valueTillHere: string): void {
-      setIsPopoverOpen(!isPopoverOpen)
-      setOpenTargetElement(isOpen, setIsOpen, valueTillHere, true)
-    }
-
-    return (
-      <Layout.Horizontal>
-        <Popover isOpen={isPopoverOpen} minimal>
-          <Layout.Horizontal className={css.visibleItemText} onClick={() => itemClickHandler(items[0].valueTillHere)}>
-            <div className={css.visibleItemTextHeader}>{items[0].value}</div>
-            <Icon name={getDropDownIcon(items[0], isOpen)} padding={{ left: '5px', right: '10px' }} size={10} />
-          </Layout.Horizontal>
-          <Menu>
-            {items[0].children.map(
-              (child, ind) =>
-                child.children.length !== 0 && (
-                  <Menu.Item
-                    key={`${ind} ${child.value}`}
-                    onClick={() => {
-                      dropDownItemClickHandler(child.valueTillHere)
-                      setOpenTargetElement(isOpen, setIsOpen, items[0].valueTillHere, false)
-                    }}
-                    text={child.value}
-                  />
-                )
-            )}
-          </Menu>
-        </Popover>
-        {items.length > 1 ? (
-          <Popover minimal>
-            <div style={{ cursor: 'pointer' }}>...</div>
-            <Menu>
-              {items.map((item, index) => {
-                if (index !== 0) {
-                  return (
-                    <Menu.Item
-                      key={index}
-                      text={item.value}
-                      onClick={() => dropDownItemClickHandler(item.valueTillHere)}
-                    />
-                  )
-                }
-              })}
-            </Menu>
-          </Popover>
-        ) : null}
-      </Layout.Horizontal>
-    )
-  }
-}
-
-function getVisibleItemRenderer(props: getVisibleItemRendererProps): any {
-  const { dropDownItemClickHandler, isOpen, setIsOpen } = props
-
-  // eslint-disable-next-line react/display-name
-  return (item: TrieNode, index: number): JSX.Element => {
-    function itemClickHandler(valueTillHere: string): void {
-      setOpenTargetElement(isOpen, setIsOpen, valueTillHere, true)
-    }
-
-    // return the visible item JSX from here
-
-    const shallShowNesting = item.children.some(child => child.children.length !== 0)
-
-    return (
-      <Popover key={index} minimal>
-        <Layout.Horizontal className={css.visibleItemText} onClick={() => itemClickHandler(item.valueTillHere)}>
-          <div className={css.visibleItemTextHeader}>{item.value}</div>
-          {shallShowNesting && <Icon name={getDropDownIcon(item, isOpen)} className={css.paddingLeft} size={10} />}
-        </Layout.Horizontal>
-        <Menu>
-          {item.children.map(
-            (child, ind) =>
-              child.children.length !== 0 && (
-                <Menu.Item
-                  key={`${ind} ${child.value}`}
-                  onClick={() => {
-                    dropDownItemClickHandler(child.valueTillHere)
-                    setOpenTargetElement(isOpen, setIsOpen, item.valueTillHere, false)
-                  }}
-                  text={child.value}
-                />
-              )
-          )}
-        </Menu>
-      </Popover>
-    )
-  }
-}
-
 export const NewExpressionDropdown = (props: NewExpressionDropdownProps): JSX.Element => {
   const { rootTrieNode, query = '', itemRenderer, setQueryValue } = props
 
-  const [overflowListItems, setOverflowListItems] = useState<TrieNode[]>([])
-
   const [isOpen, setIsOpen] = useState<boolean[]>([])
+  const [isPopoverOpen, setIsPopoverOpen] = React.useState<boolean>(false)
 
-  React.useEffect(() => {
+  const overflowListItems = React.useMemo(() => {
     // set list items here and get the correct currentTrieNode
     let currentNode: TrieNode | undefined = rootTrieNode
     const listItems: TrieNode[] = [currentNode]
@@ -159,18 +55,115 @@ export const NewExpressionDropdown = (props: NewExpressionDropdownProps): JSX.El
         currentNode = undefined
       }
     })
+    return listItems
+  }, [rootTrieNode, query])
 
-    setOverflowListItems(listItems)
-  }, [query])
+  const dropDownItemClickHandler = React.useCallback((value: string): void => {
+    setQueryValue(prevQuery => {
+      const valueBefore = prevQuery.substring(0, prevQuery.lastIndexOf('<'))
+      return `${valueBefore}<+${value}`
+    })
+  }, [])
 
-  const dropDownItemClickHandler = (value: string): void => {
-    const valueBefore = query.substring(0, query.lastIndexOf('<'))
-    setQueryValue(`${valueBefore}<+${value}`)
-  }
+  const itemClickHandler = React.useCallback(
+    (valueTillHere: string, isOverflow = false): void => {
+      setOpenTargetElement(isOpen, setIsOpen, valueTillHere, true)
+      isOverflow && setIsPopoverOpen(prevState => !prevState)
+    },
+    [isOpen, setIsOpen]
+  )
 
-  const visibleItemRenderer = getVisibleItemRenderer({ dropDownItemClickHandler, isOpen, setIsOpen })
+  const showDropdownIcon = (item: TrieNode) => item.children.some(child => child.children.length !== 0)
 
-  const overflowListRenderer = getOverflowRenderer({ dropDownItemClickHandler, isOpen, setIsOpen })
+  const visibleItemRenderer = React.useMemo(() => {
+    const VisibleItem = (item: TrieNode, index: number): JSX.Element => {
+      const clickHandler = () => itemClickHandler(item.valueTillHere)
+
+      // return the visible item JSX from here
+      const content = (
+        <Layout.Horizontal className={css.visibleItemText} onClick={clickHandler}>
+          <div className={css.visibleItemTextHeader}>{item.value}</div>
+          {showDropdownIcon(item) && (
+            <Icon name={getDropDownIcon(item, isOpen)} className={css.paddingLeft} size={10} />
+          )}
+        </Layout.Horizontal>
+      )
+
+      return (
+        <Popover key={index} minimal>
+          {content}
+          <Menu>
+            {item.children.map(
+              (child, ind) =>
+                child.children.length !== 0 && (
+                  <Menu.Item
+                    key={`${ind} ${child.value}`}
+                    onClick={() => {
+                      dropDownItemClickHandler(child.valueTillHere)
+                      setOpenTargetElement(isOpen, setIsOpen, item.valueTillHere, false)
+                    }}
+                    text={child.value}
+                  />
+                )
+            )}
+          </Menu>
+        </Popover>
+      )
+    }
+    return VisibleItem
+  }, [dropDownItemClickHandler, isOpen, setIsOpen])
+
+  const overflowListRenderer = React.useMemo(() => {
+    const OverflowList = (items: TrieNode[]): JSX.Element => {
+      const clickHandler = () => itemClickHandler(items[0].valueTillHere, true)
+
+      return (
+        <Layout.Horizontal>
+          <Popover isOpen={isPopoverOpen} minimal>
+            <Layout.Horizontal className={css.visibleItemText} onClick={clickHandler}>
+              <div className={css.visibleItemTextHeader}>{items[0].value}</div>
+              <Icon name={getDropDownIcon(items[0], isOpen)} padding={{ left: '5px', right: '10px' }} size={10} />
+            </Layout.Horizontal>
+            <Menu>
+              {items[0].children.map(
+                (child, ind) =>
+                  child.children.length !== 0 && (
+                    <Menu.Item
+                      key={`${ind} ${child.value}`}
+                      onClick={() => {
+                        dropDownItemClickHandler(child.valueTillHere)
+                        setOpenTargetElement(isOpen, setIsOpen, items[0].valueTillHere, false)
+                      }}
+                      text={child.value}
+                    />
+                  )
+              )}
+            </Menu>
+          </Popover>
+          {items.length > 1 ? (
+            <Popover minimal>
+              <div style={{ cursor: 'pointer' }}>...</div>
+              <Menu>
+                {items.map((item, index) => {
+                  if (index !== 0) {
+                    return (
+                      <Menu.Item
+                        key={index}
+                        text={item.value}
+                        onClick={() => dropDownItemClickHandler(item.valueTillHere)}
+                      />
+                    )
+                  }
+                })}
+              </Menu>
+            </Popover>
+          ) : null}
+        </Layout.Horizontal>
+      )
+    }
+
+    return OverflowList
+  }, [dropDownItemClickHandler, isOpen, setIsOpen])
 
   const queryValue = query.substring(query.lastIndexOf('+') + 1)
 
