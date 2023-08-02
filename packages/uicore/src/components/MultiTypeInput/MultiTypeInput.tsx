fb9927a4890b39ba1ec7f6305d077f60496f9d35
Chetan Vekariya <chetan.vekariya@harness.io>
2023-08-02 18:29:35 +0530

    fix: [CDS-76216]: set default value of defaultValueToReset as empty string in ExpressionAndRuntimeType component (#1124)
    * fix: set default value of defaultValueToReset as empty string in ExpressionAndRuntimeType component --no-verify
    * test: added test case to cover change

--- packages/uicore/src/components/MultiTypeInput/MultiTypeInput.tsx
+++ packages/uicore/src/components/MultiTypeInput/MultiTypeInput.tsx
@@ -122,7 +122,7 @@ export const getMultiTypeFromValue = (
 export function ExpressionAndRuntimeType<T = unknown>(props: ExpressionAndRuntimeTypeProps<T>): React.ReactElement {
   const {
     value,
-    defaultValueToReset,
+    defaultValueToReset = '',
     width,
     expressions = [],
     onTypeChange,
