7ba296b01be27a663a3305fb0e5cd68546be740f
Shaurya Kalia <113332561+shaurya-harness@users.noreply.github.com>
2023-10-18 12:32:20 +0530

    feat: [CI-9926]: icon for GAR step (#1179)
    * feat: icon for GAR step
    
    * feat: icon for GAR step

--- packages/icons/src/HarnessIcons.ts
+++ packages/icons/src/HarnessIcons.ts
@@ -529,6 +529,7 @@ import FsRootFolder from './fs-root-folder.svg'
 import FullScreenExit from './full-screen-exit.svg'
 import FullScreen from './full-screen.svg'
 import Functions from './functions.svg'
+import GarStep from './gar-step.svg'
 import GcpEngine from './gcp-engine.svg'
 import GcpKms from './gcp-kms.svg'
 import GcpSecretManager from './gcp-secret-manager.svg'
@@ -1776,6 +1777,7 @@ type HarnessIconName =
   | 'full-screen-exit'
   | 'full-screen'
   | 'functions'
+  | 'gar-step'
   | 'gcp-engine'
   | 'gcp-kms'
   | 'gcp-secret-manager'
@@ -3023,6 +3025,7 @@ const HarnessIcons: KVO<ElementType> = {
   'full-screen-exit': FullScreenExit,
   'full-screen': FullScreen,
   functions: Functions,
+  'gar-step': GarStep,
   'gcp-engine': GcpEngine,
   'gcp-kms': GcpKms,
   'gcp-secret-manager': GcpSecretManager,
