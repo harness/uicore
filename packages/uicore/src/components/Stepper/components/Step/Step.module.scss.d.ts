6f0474884524661f9501b031dba8d3b3c9cf9b85
deepesh-ui <81554936+deepesh-ui@users.noreply.github.com>
2023-10-31 08:35:03 +0530

    feat: [SRM-16021]: Added Stepper component (#1185)
    * feat: [SRM-16021]: Added Stepper component
    
    * feat: [SRM-16021]: fixed eslint error

---
+++ packages/uicore/src/components/Stepper/components/Step/Step.module.scss.d.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+/**
+ * Copyright 2021 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ **/
+// this is an auto-generated file, do not update this manually
+declare const styles: {
+  readonly alignContainerRight: string
+  readonly borderLeft: string
+  readonly bottomMargin: string
+  readonly card: string
+  readonly stepContainer: string
+  readonly stepMargin: string
+  readonly stepSubtitle: string
+}
+export default styles
---
+++ packages/uicore/src/components/Stepper/components/Step/components/StepTitle/StepTitle.tsx
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import React from 'react'
+import { noop } from 'lodash-es'
+import cx from 'classnames'
+import { Icon } from '@harness/icons'
+import { FontVariation } from '@harness/design-system'
+import type { StepTitleInterface } from './StepTitle.types'
+import { getStateByStatus } from './StepTitle.utils'
+import { StepStatus } from '../../Step.constants'
+import { Text, Layout } from '../../../../../../'
+import css from './StepTitle.module.css'
+
+export const StepTitle = ({
+  step,
+  index,
+  isCurrent,
+  stepStatus,
+  onClick,
+  isOptional,
+  hideTitle
+}: StepTitleInterface): JSX.Element => {
+  const isErrorOrSuccess = stepStatus !== StepStatus.INCONCLUSIVE
+  const { icon, labelColor, iconColor, cursor } = getStateByStatus(stepStatus)
+  return (
+    <Layout.Vertical spacing="small">
+      <Layout.Horizontal
+        data-testid={`steptitle_${step.id}`}
+        style={{ cursor: isErrorOrSuccess ? 'pointer' : cursor }}
+        key={`${step.id}_horizontal`}
+        onClick={() => (isErrorOrSuccess ? onClick(index) : noop)}
+        flex={{ alignItems: 'center', justifyContent: 'start' }}>
+        {isCurrent ? (
+          <Icon
+            name={isErrorOrSuccess ? icon : 'Edit'}
+            size={isErrorOrSuccess ? 20 : 16}
+            margin="small"
+            color={iconColor}
+            className={cx(css.iconNoBorder, {
+              [css.editIcon]: !isErrorOrSuccess
+            })}
+          />
+        ) : (
+          <Icon
+            name={icon}
+            size={20}
+            margin="small"
+            color={iconColor}
+            className={cx(css.iconNoBorder, !isErrorOrSuccess && css.defaultState)}
+          />
+        )}
+        {!hideTitle && (
+          <Text font={{ variation: FontVariation.H5 }} color={labelColor}>
+            {step.title} {isOptional && '(Optional)'}
+          </Text>
+        )}
+      </Layout.Horizontal>
+    </Layout.Vertical>
+  )
+}
