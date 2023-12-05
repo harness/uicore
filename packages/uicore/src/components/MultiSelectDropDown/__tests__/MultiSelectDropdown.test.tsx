c6534288f46fb4d6864263c326b49c42886df141
crystal-fong <95680496+crystal-fong@users.noreply.github.com>
2023-12-05 14:50:15 +0000

    feat: [FFM-10188]: multiselectdropdown prop (#1204)
    * feat: [FFM-10188]:
    wip multiselectdropdown new prop
    
    * feat: [FFM-10188]: bumping version
    
    * feat: [FFM-10188]: tidied up test suite of multi select dropdown
    
    * feat: [FFM-10188]: simplified the optional chaining
    
    * feat: [FFM-10188]: readded in removed test

---
+++ packages/uicore/src/components/MultiSelectDropDown/__tests__/MultiSelectDropdown.test.tsx
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2023 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import React from 'react'
+import { render, screen, RenderResult, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { MultiSelectDropDown, MultiSelectDropDownProps } from '../MultiSelectDropDown'
+
+const renderComponent = (props: Partial<MultiSelectDropDownProps> = {}): RenderResult =>
+  render(<MultiSelectDropDown items={[]} {...props} />)
+
+describe('MultiSelectDropDown', () => {
+  test('it should display items passed through as props', async () => {
+    renderComponent({
+      items: [
+        { label: 'value1', value: 'value1id' },
+        { label: 'value2', value: 'value2id' }
+      ],
+      allowSearch: true
+    })
+
+    userEvent.click(screen.getByText('Select'))
+
+    expect(screen.getByRole('checkbox', { name: 'value1' })).toBeInTheDocument()
+    expect(screen.getByRole('checkbox', { name: 'value2' })).toBeInTheDocument()
+  })
+
+  test('it should not display ExpandingSearchInput if allowSearch prop is false', async () => {
+    renderComponent({
+      items: [],
+      allowSearch: false
+    })
+
+    userEvent.click(screen.getByText('Select'))
+
+    expect(screen.queryByRole('searchbox')).not.toBeInTheDocument()
+  })
+
+  test('it should filter out non-matching results when a user performs a search', async () => {
+    renderComponent({
+      items: [
+        { label: 'value1', value: 'value1id' },
+        { label: 'value2', value: 'value2id' }
+      ],
+      allowSearch: true,
+      usePortal: true
+    })
+
+    userEvent.click(screen.getByText('Select'))
+
+    expect(screen.getByRole('checkbox', { name: 'value1' })).toBeInTheDocument()
+    expect(screen.getByRole('checkbox', { name: 'value2' })).toBeInTheDocument()
+
+    const dropdownSearchbox = await screen.findByRole('searchbox')
+
+    const dropdownInput = 'value1'
+
+    userEvent.type(dropdownSearchbox, dropdownInput)
+
+    expect(dropdownSearchbox).toHaveValue(dropdownInput)
+
+    expect(await screen.findByText(dropdownInput)).toBeInTheDocument()
+    await waitFor(() => expect(screen.queryByText('value2')).not.toBeInTheDocument())
+  })
+
+  test('it should call ExpandingSearchInputProps onChange with the correct value when user performs a search when that prop is defined', async () => {
+    const onSearchChange = jest.fn()
+
+    renderComponent({
+      items: [
+        { label: 'value1', value: 'value1id' },
+        { label: 'value2', value: 'value2id' }
+      ],
+      allowSearch: true,
+      expandingSearchInputProps: {
+        onChange: onSearchChange
+      },
+      usePortal: true
+    })
+
+    userEvent.click(screen.getByText('Select'))
+
+    const dropdownSearchbox = await screen.findByRole('searchbox')
+
+    const dropdownInput = 'value1'
+
+    await waitFor(() => expect(onSearchChange).not.toHaveBeenCalled())
+
+    userEvent.type(dropdownSearchbox, dropdownInput)
+
+    expect(dropdownSearchbox).toHaveValue(dropdownInput)
+
+    await waitFor(() => expect(onSearchChange).toHaveBeenCalledWith(dropdownInput))
+  })
+})
