eca8fb1980243192f0bb61396975d3e07b650920
Ramesh Reddy <ramesh.reddy@harness.io>
2023-12-14 11:34:07 +0530

    feat: [CDS-85919]: Add runtime input renderer to `ExpressionAndRuntimeType` (#1208)
    * feat: [CDS-85919]: Add runtime input renderer to `ExpressionAndRuntimeType`
    
    * feat: [CDS-85919]: Bump version
    
    * feat: [CDS-85919]: Change Y1 to V1

--- packages/uicore/src/components/MultiTypeInput/LearnMorePopover.tsx
+++ packages/uicore/src/components/MultiTypeInput/LearnMorePopover.tsx
@@ -35,6 +35,11 @@ const helperText: Record<MultiTypeInputType, React.ReactNode> = {
       <b>Runtime Inputs</b> are placeholders for values that will be provided when you start a Pipeline execution.
     </React.Fragment>
   ),
+  [MultiTypeInputType.RUNTIMEV1]: (
+    <React.Fragment>
+      <b>Runtime Inputs</b> are placeholders for values that will be provided when you start a Pipeline execution.
+    </React.Fragment>
+  ),
   [MultiTypeInputType.EXECUTION_TIME]: (
     <React.Fragment>
       <b>Execution Inputs</b> are placeholders for values that must be provided when manually initiating a pipeline
@@ -53,6 +58,7 @@ export const labels: Record<MultiTypeInputType, string> = {
   [MultiTypeInputType.EXPRESSION]: 'expression',
   [MultiTypeInputType.FIXED]: 'fixedValue',
   [MultiTypeInputType.RUNTIME]: 'runtimeInput',
+  [MultiTypeInputType.RUNTIMEV1]: 'runtimeInput',
   [MultiTypeInputType.EXECUTION_TIME]: 'executionTimeInput',
   [MultiTypeInputType.REGEX]: 'regex'
 }
