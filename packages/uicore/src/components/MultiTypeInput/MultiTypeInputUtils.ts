fd31896222502f829eeffa1a4fb9f95766b08587
Ramesh Reddy <ramesh.reddy@harness.io>
2023-12-14 11:34:07 +0530

    feat: [CDS-85919]: Add runtime input renderer to `ExpressionAndRuntimeType` (#1208)
    * feat: [CDS-85919]: Add runtime input renderer to `ExpressionAndRuntimeType`
    
    * feat: [CDS-85919]: Bump version
    
    * feat: [CDS-85919]: Change Y1 to V1

--- packages/uicore/src/components/MultiTypeInput/MultiTypeInputUtils.ts
+++ packages/uicore/src/components/MultiTypeInput/MultiTypeInputUtils.ts
@@ -10,6 +10,7 @@ import type { IconName } from '@harness/icons'
 export enum MultiTypeInputType {
   FIXED = 'FIXED',
   RUNTIME = 'RUNTIME',
+  RUNTIMEV1 = 'RUNTIMEV1',
   EXECUTION_TIME = 'EXECUTION_TIME',
   EXPRESSION = 'EXPRESSION',
   REGEX = 'REGEX'
@@ -24,6 +25,7 @@ export enum MultiTypeInputValue {
 export const MultiTypeIcon: Record<MultiTypeInputType, IconName> = {
   FIXED: 'fixed-input',
   RUNTIME: 'runtime-input',
+  RUNTIMEV1: 'runtime-input',
   EXECUTION_TIME: 'runtime-input',
   EXPRESSION: 'expression-input',
   REGEX: 'reg-ex'
@@ -32,12 +34,14 @@ export const MultiTypeIcon: Record<MultiTypeInputType, IconName> = {
 export const MultiTypeIconSize: Record<MultiTypeInputType, number> = {
   FIXED: 12,
   RUNTIME: 12,
+  RUNTIMEV1: 12,
   EXECUTION_TIME: 12,
   EXPRESSION: 16,
   REGEX: 16
 }
 
 export const RUNTIME_INPUT_VALUE = '<+input>'
+export const RUNTIME_INPUT_V1_PREFIX = '<+inputs.'
 export const EXECUTION_TIME_INPUT_VALUE = '<+input>.executionInput()'
 export const EXPRESSION_INPUT_PLACEHOLDER = '<+expression>'
 export const REGEX_INPUT_PLACEHOLDER = '^match$'
