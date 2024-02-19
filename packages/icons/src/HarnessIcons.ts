1c78d4931ca7e3b93f071b4142b4498d09a64eef
Deepesh Kumar <81554936+deepesh-ui@users.noreply.github.com>
2024-02-19 09:25:28 +0530

    feat: [SSCA-1405]: SSCA execution view icons (#1241)
    * feat: [SSCA-1405]: SSCA execution view icons
    
    * feat: [SSCA-1405]: SSCA execution view icons
    
    * feat: [SSCA-1405]: SSCA execution view icons

--- packages/icons/src/HarnessIcons.ts
+++ packages/icons/src/HarnessIcons.ts
@@ -1015,6 +1015,7 @@ import SaveCacheS3StepInverse from './save-cache-s3-step-inverse.svg'
 import SaveCacheS3Step from './save-cache-s3-step.svg'
 import SaveCacheS3 from './save-cache-s3.svg'
 import SaveCacheStep from './save-cache-step.svg'
+import SbomDriftDetected from './sbom-drift-detected.svg'
 import Scm from './scm.svg'
 import Script from './script.svg'
 import SearchApplications from './search-applications.svg'
@@ -1159,6 +1160,7 @@ import Slo from './slo.svg'
 import SlotDeployment from './slot-deployment.svg'
 import SlsaGeneration from './slsa-generation.svg'
 import SlsaVerification from './slsa-verification.svg'
+import Slsa from './slsa.svg'
 import SmtpConfigurationBlue from './smtp-configuration-blue.svg'
 import Smtp from './smtp.svg'
 import SolidError from './solid-error.svg'
@@ -2319,6 +2321,7 @@ type HarnessIconName =
   | 'save-cache-s3-step'
   | 'save-cache-s3'
   | 'save-cache-step'
+  | 'sbom-drift-detected'
   | 'scm'
   | 'script'
   | 'search-applications'
@@ -2463,6 +2466,7 @@ type HarnessIconName =
   | 'slot-deployment'
   | 'slsa-generation'
   | 'slsa-verification'
+  | 'slsa'
   | 'smtp-configuration-blue'
   | 'smtp'
   | 'solid-error'
@@ -3623,6 +3627,7 @@ const HarnessIcons: KVO<ElementType> = {
   'save-cache-s3-step': SaveCacheS3Step,
   'save-cache-s3': SaveCacheS3,
   'save-cache-step': SaveCacheStep,
+  'sbom-drift-detected': SbomDriftDetected,
   scm: Scm,
   script: Script,
   'search-applications': SearchApplications,
@@ -3767,6 +3772,7 @@ const HarnessIcons: KVO<ElementType> = {
   'slot-deployment': SlotDeployment,
   'slsa-generation': SlsaGeneration,
   'slsa-verification': SlsaVerification,
+  slsa: Slsa,
   'smtp-configuration-blue': SmtpConfigurationBlue,
   smtp: Smtp,
   'solid-error': SolidError,
