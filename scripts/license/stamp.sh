35a3c99c7e76e82e490d51daf79c1da4d52064b4
Sahil <sahil.kumar@harness.io>
2024-03-21 23:04:06 +0530

    chore: [PL-48492]: updated stamp file to conditionally stamp licenses based on file path
    Signed-off-by: Sahil <sahil.kumar@harness.io>

--- scripts/license/stamp.sh
+++ scripts/license/stamp.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-# Copyright (c) Harness Inc.
-#
-# This source code is licensed under the Apache 2.0 license found in the
-# LICENSE file in the root directory of this source tree.
+# Copyright 2024 Harness Inc. All rights reserved.
+# Use of this source code is governed by the PolyForm Shield 1.0.0 license
+# that can be found in the licenses directory at the root of this repository, also available at
+# https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
 
 SCRIPT_DIR=$(dirname "$0")
 
 STAGED_FILES=$(tr ' ' '\n' <<< "$@")
 
 while read -r filepath; do
-  "$SCRIPT_DIR"/add_license_header.sh -l "$SCRIPT_DIR/.license-header-apache-2-0.txt" -f "$filepath"
+  if [[ $filepath == *"/packages/uicore"* || $filepath == *"/packages/icons"* ]]; then
+    LICENSE_HEADER_FILE="$SCRIPT_DIR/.license-header-apache-2-0.txt"
+  else
+    LICENSE_HEADER_FILE="$SCRIPT_DIR/.license-header-polyform-shield.txt"
+  fi
+
+  "$SCRIPT_DIR"/add_license_header.sh -l "$LICENSE_HEADER_FILE" -f "$filepath"
 done <<< "$STAGED_FILES"
