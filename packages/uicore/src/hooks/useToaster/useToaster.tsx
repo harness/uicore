cd770f02730c807b5fac018613f0c51995c90322
Pratyush Garg <pratyush.garg@harness.io>
2024-04-23 16:09:43 +0530

    fix: [PIPE-18595]: hide toast if message is empty string

--- packages/uicore/src/hooks/useToaster/useToaster.tsx
+++ packages/uicore/src/hooks/useToaster/useToaster.tsx
@@ -6,7 +6,7 @@
  */
 
 import type { ReactNode } from 'react'
-import { Toaster, Position, IToaster, Intent } from '@blueprintjs/core'
+import { Toaster, Position, IToaster, Intent, IToastProps } from '@blueprintjs/core'
 import css from './useToaster.css'
 
 const toaster = Toaster.create({
@@ -21,20 +21,26 @@ export interface ToasterProps extends IToaster {
   showPrimary: (message: string | ReactNode, timeout?: number, key?: string) => void
 }
 
+const showToast = (options: IToastProps, key?: string): void => {
+  if (options.message !== '') {
+    toaster.show(options, key)
+  }
+}
+
 const showSuccess = (message: string | ReactNode, timeout?: number, key?: string): void => {
-  toaster.show({ message, intent: Intent.SUCCESS, icon: 'tick', timeout }, key)
+  showToast({ message, intent: Intent.SUCCESS, icon: 'tick', timeout }, key)
 }
 
 const showError = (message: string | ReactNode, timeout?: number, key?: string): void => {
-  toaster.show({ message, intent: Intent.DANGER, icon: 'warning-sign', timeout }, key)
+  showToast({ message, intent: Intent.DANGER, icon: 'warning-sign', timeout }, key)
 }
 
 const showWarning = (message: string | ReactNode, timeout?: number, key?: string): void => {
-  toaster.show({ message, intent: Intent.WARNING, icon: 'error', timeout }, key)
+  showToast({ message, intent: Intent.WARNING, icon: 'error', timeout }, key)
 }
 
 const showPrimary = (message: string | ReactNode, timeout?: number, key?: string): void => {
-  toaster.show({ message, intent: Intent.PRIMARY, timeout }, key)
+  showToast({ message, intent: Intent.PRIMARY, timeout }, key)
 }
 
 export function useToaster(): ToasterProps {
