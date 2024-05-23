7df32a9e139bbf41b77abcd3aadf6bcaaf66b562
Pankaj Singh <110591046+pankajsingh-harness@users.noreply.github.com>
2024-05-23 14:58:08 +0530

    feat: [PIPE-18812}]: Add support for passing content to ListHeader (#1318)
    * feat: [PIPE-18812}]: Add support for passing content to ListHeader
    
    * feat: [PIPE-18812]: Fix review comment

---
+++ packages/uicore/src/components/ListHeader/ListHeader.stories.tsx
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2024 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import React from 'react'
+import type { Meta, Story } from '@storybook/react'
+import { Title, Subtitle, ArgsTable, Stories, PRIMARY_STORY, Primary } from '@storybook/addon-docs/blocks'
+import {
+  Layout,
+  ListHeader,
+  sortByName,
+  sortByEmail,
+  sortByCreated,
+  sortByStatus,
+  sortByVersion,
+  SortMethod,
+  Select
+} from '../..'
+import { ListHeaderProps } from './ListHeader'
+import { IconName } from '@blueprintjs/core'
+import css from './ListHeaderStory.css'
+import cx from 'classnames'
+import { noop } from 'lodash-es'
+
+export default {
+  title: 'Components / ListHeader',
+
+  component: ListHeader,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      source: {
+        type: 'code'
+      },
+
+      page: function PageDescription() {
+        return (
+          <>
+            <Title>ListHeader</Title>
+            <Subtitle>
+              <code>{`import {ListHeader} from '@harness/uicore'`}</code>
+            </Subtitle>
+            <Primary />
+            <ArgsTable story={PRIMARY_STORY} />
+
+            <Stories />
+          </>
+        )
+      }
+    }
+  },
+  decorators: [
+    Story => (
+      <Layout.Horizontal spacing="small">
+        <Story />
+      </Layout.Horizontal>
+    )
+  ]
+} as Meta
+
+export const ListHeaderWithoutContent: Story<ListHeaderProps> = () => {
+  return (
+    <ListHeader
+      totalCount={100}
+      sortOptions={[...sortByName, ...sortByEmail, ...sortByCreated, ...sortByStatus, ...sortByVersion]}
+      selectedSortMethod={SortMethod.NameAsc}
+      onSortMethodChange={() => {
+        //
+      }}
+      className={cx(css.listHeaderStory)}
+    />
+  )
+}
+
+export const ListHeaderWithContent: Story<ListHeaderProps> = () => {
+  const items = [
+    {
+      label: 'Service Kubernetes',
+      value: 'service-kubernetes',
+      icon: { name: 'service-kubernetes' as IconName }
+    },
+    {
+      label: 'Service Github',
+      value: 'service-github',
+      icon: { name: 'service-github' as IconName }
+    },
+    { label: 'ELK', value: 'service-elk', icon: { name: 'service-elk' as IconName } },
+    { label: 'Jenkins', value: 'service-jenkins', icon: { name: 'service-jenkins' as IconName } },
+    { label: 'GCP', value: 'service-gcp', icon: { name: 'service-gcp' as IconName } }
+  ]
+  return (
+    <ListHeader
+      totalCount={100}
+      sortOptions={[...sortByName, ...sortByEmail, ...sortByCreated, ...sortByStatus, ...sortByVersion]}
+      selectedSortMethod={SortMethod.NameAsc}
+      onSortMethodChange={noop}
+      content={
+        <div style={{ width: '240px', marginRight: '12px' }}>
+          <Select items={items} addClearBtn={true} value={items[1]} />
+        </div>
+      }
+      className={cx(css.listHeaderStory)}
+    />
+  )
+}
