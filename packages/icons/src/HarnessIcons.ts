1c78419661e842c29303575198ff4b947088eb1a
Naman Gupta <naman.gupta@harness.io>
2025-03-11 08:33:42 +0000

    feat: [PL-60343]: add impersonate icon (#1526)
    * feat: [PL-60343]: add impersonate icon

--- packages/icons/src/HarnessIcons.ts
+++ packages/icons/src/HarnessIcons.ts
@@ -890,6 +890,7 @@ import IgnoreFailed from './ignoreFailed.svg'
 import ImageAppLayer from './image-app-layer.svg'
 import ImageBaseLayer from './image-base-layer.svg'
 import ImageDistroLayer from './image-distro-layer.svg'
+import Impersonate from './impersonate.svg'
 import Incidents from './incidents.svg'
 import Infinity from './infinity.svg'
 import InfinityTrend from './infinityTrend.svg'
@@ -2526,6 +2527,7 @@ type HarnessIconName =
   | 'image-app-layer'
   | 'image-base-layer'
   | 'image-distro-layer'
+  | 'impersonate'
   | 'incidents'
   | 'infinity'
   | 'infinityTrend'
@@ -4162,6 +4164,7 @@ const HarnessIcons: KVO<ElementType> = {
   'image-app-layer': ImageAppLayer,
   'image-base-layer': ImageBaseLayer,
   'image-distro-layer': ImageDistroLayer,
+  impersonate: Impersonate,
   incidents: Incidents,
   infinity: Infinity,
   infinityTrend: InfinityTrend,
