827d7e1e057864cc7ffba2b31517eabc9ae9433f
Avinash Madhwani <avinash.madhwani@harness.io>
2025-07-29 13:21:05 +0000

    fix: [ML-1165]: Improved AI split dropdown button click area (#1625)
    * fix: removed margin left and adding padding
    
    * fix: [ML-1165]: fixed targetting of ai split button dropdown

--- packages/uicore/src/components/SplitButton/SplitButton.tsx
+++ packages/uicore/src/components/SplitButton/SplitButton.tsx
@@ -83,6 +83,7 @@ export const SplitButton: React.FC<SplitButtonProps> = ({
           withoutCurrentColor
           variation={variation}
           {...commonProps}
+          className={css.aiButton}
           tooltip={tooltip}
           disabled={disabled}
           onClick={e => {
