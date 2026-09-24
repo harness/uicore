29fe1aab725d33a24ad740584b190c1ea4261dff
Shubham Chaudhary <shubham.chaudhary@harness.io>
2026-09-24 12:23:25 +0530

    chore: [CHAOS-13433]: fix the PR checks
    Signed-off-by: Shubham Chaudhary <shubham.chaudhary@harness.io>

--- packages/icons/src/HarnessIcons.ts
+++ packages/icons/src/HarnessIcons.ts
@@ -504,8 +504,8 @@ import ChaosStepExperimentsPipeline from './chaos-step-experiments-pipeline.svg'
 import ChaosStepFaultsPipeline from './chaos-step-faults-pipeline.svg'
 import ChaosStepLoadPipeline from './chaos-step-load-pipeline.svg'
 import ChaosStepProbesPipeline from './chaos-step-probes-pipeline.svg'
-import ChaosStepRiskScanPipeline from './chaos-step-risk-scan-pipeline.svg'
 import ChaosStepProbes from './chaos-step-probes.svg'
+import ChaosStepRiskScanPipeline from './chaos-step-risk-scan-pipeline.svg'
 import ChaosVmwareCpuHog from './chaos-vmware-cpu-hog.svg'
 import ChaosVmwareDiskLoss from './chaos-vmware-disk-loss.svg'
 import ChaosVmwareDnsChaos from './chaos-vmware-dns-chaos.svg'
@@ -2486,8 +2486,8 @@ type HarnessIconName =
   | 'chaos-step-faults-pipeline'
   | 'chaos-step-load-pipeline'
   | 'chaos-step-probes-pipeline'
-  | 'chaos-step-risk-scan-pipeline'
   | 'chaos-step-probes'
+  | 'chaos-step-risk-scan-pipeline'
   | 'chaos-vmware-cpu-hog'
   | 'chaos-vmware-disk-loss'
   | 'chaos-vmware-dns-chaos'
@@ -4468,8 +4468,8 @@ const HarnessIcons: KVO<ElementType> = {
   'chaos-step-faults-pipeline': ChaosStepFaultsPipeline,
   'chaos-step-load-pipeline': ChaosStepLoadPipeline,
   'chaos-step-probes-pipeline': ChaosStepProbesPipeline,
-  'chaos-step-risk-scan-pipeline': ChaosStepRiskScanPipeline,
   'chaos-step-probes': ChaosStepProbes,
+  'chaos-step-risk-scan-pipeline': ChaosStepRiskScanPipeline,
   'chaos-vmware-cpu-hog': ChaosVmwareCpuHog,
   'chaos-vmware-disk-loss': ChaosVmwareDiskLoss,
   'chaos-vmware-dns-chaos': ChaosVmwareDnsChaos,
